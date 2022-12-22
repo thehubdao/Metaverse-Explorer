e53227e0945c209ce80893c718985d7753fe6d1b
Laura Sandoval <laurad00sandovalg@gmail.com>
2022-12-22 12:51:03 -0500

    LOADER UPDATED
    Add animation for loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,7 +1,7 @@
 const Loader = () => {
     return (
         <div className="w-full h-full self-center flex items-center justify-center">
-            <img src="/images/mgh_logo.svg" className={` h-24 w-24 logo`} />
+            <img src="/images/mgh_logo.svg" className={` h-24 w-24 animate-bounce`} />
         </div>
     )
 }
