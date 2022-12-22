754cf04410326d9e8c9b1d90dadf9490adfda4c2
Laura Sandoval <laurad00sandovalg@gmail.com>
2022-12-22 12:54:52 -0500

    NFT CARD UPDATED
    Fix responsive bug for nft card

--- components/nftValuation/NftCard.tsx
+++ components/nftValuation/NftCard.tsx
@@ -4,9 +4,9 @@ const NftCard = ({ image, value, text }: any) => {
 
     return (
         <div
-            className={`grid grid-rows-3 rounded-xl cursor-pointer w-[240px] h-[360px] focus:outline-none nm-flat-hard  hover:nm-flat-soft transition duration-300 ease-in-out`}
+            className={`grid grid-rows-3 rounded-xl cursor-pointer lg:w-[200px] lg:h-[300px] 2xl:w-[240px] 2xl:h-[360px] focus:outline-none nm-flat-hard  hover:nm-flat-soft transition duration-300 ease-in-out`}
         >
-            <div className="w-full h-full relative row-span-2">
+            <div className="relative row-span-2">
                 <Image
                     src={image}
                     loading='lazy'
