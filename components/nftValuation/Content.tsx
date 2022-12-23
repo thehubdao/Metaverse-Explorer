0ebd7e306db17bc0c36304ac99999f14ab70e48c
Laura Sandoval <laurad00sandovalg@gmail.com>
2022-12-23 13:13:25 -0500

    NFT COMMON TYPES UPDATED
    Object global updated

--- components/nftValuation/Content.tsx
+++ components/nftValuation/Content.tsx
@@ -1,117 +1,125 @@
+import { useEffect, useState } from "react";
 import Loader from "../Loader";
 import Pagination from "../Pagination";
 import NftCard from "./NftCard";
 
 interface nftObject {
-  tokenId: string
-  floor_adjusted_predicted_price: number
-  traits: {
-    traitType: string
-    value: string
-  }[]
-  images: {
-    image_small: string
-  }
+	tokenId: string;
+	floor_adjusted_predicted_price: number;
+	traits: {
+		traitType: string;
+		value: string;
+	}[];
+	images: {
+		image_small: string;
+	};
 }
 
 interface ContentProps {
-  filteredItem: nftObject[]
-  checked: number | undefined
-  nftObject: nftObject[]
-  controlPageIndex: number
-  pageLenght: number
-  setControlPageIndex: Function
-  isLoading: boolean
+	filteredItem: nftObject[];
+	checked: boolean;
+	nftObject: nftObject[];
+	isLoading: boolean;
 }
 
 const formatter = new Intl.NumberFormat("en-US", {
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 4,
+	minimumFractionDigits: 2,
+	maximumFractionDigits: 4,
 });
 
 export default function Content({
-  filteredItem,
-  checked,
-  nftObject,
-  controlPageIndex,
-  pageLenght,
-  setControlPageIndex,
-  isLoading
+	filteredItem,
+	checked,
+	nftObject,
+	isLoading,
 }: ContentProps) {
+	// Pagination Controller
+	const [pageLenght, setPageLenght] = useState(0);
+	const [controlPageIndex, setControlPageIndex] = useState<number>(0);
 
-  const dataFluf = () => {
-    const flufs: React.ReactElement[] = [];
-    for (
-      let index: number = controlPageIndex * 10;
-      index < controlPageIndex * 10 + 20;
-      index++
-    ) {
-      if (!nftObject[index]) return flufs;
-      flufs.push(
-        <NftCard
-          image={nftObject[index]["images"]["image_small"]}
-          text="Estimated Price: "
-          value={formatter.format(
-            nftObject[index]["floor_adjusted_predicted_price"]
-          )}
-          key={index}
-        />
-      );
-    }
-    return flufs;
-  }
+	useEffect(() => {
+		if (filteredItem.length > 0) {
+			setPageLenght(Math.trunc(filteredItem.length / 10));
+			setControlPageIndex(0);
+		} else {
+			setPageLenght(Math.trunc(nftObject?.length / 10));
+			setControlPageIndex(0);
+		}
+	}, [filteredItem, nftObject]);
 
-  const datafiltered = () => {
-    const flufs: React.ReactElement[] = [];
-    for (
-      let index: number = controlPageIndex * 10;
-      index < controlPageIndex * 10 + 20;
-      index++
-    ) {
-      if (!filteredItem[index]) return flufs;
-      flufs.push(
-        <NftCard
-          image={filteredItem[index]["images"]["image_small"]}
-          text="Estimated Price: "
-          value={formatter.format(
-            filteredItem[index]["floor_adjusted_predicted_price"]
-          )}
-          key={index}
-        />
-      );
-    }
-    return flufs;
-  }
+	const dataFluf = () => {
+		const flufs: React.ReactElement[] = [];
+		for (
+			let index: number = controlPageIndex * 10;
+			index < controlPageIndex * 10 + 20;
+			index++
+		) {
+			if (!nftObject[index]) return flufs;
+			flufs.push(
+				<NftCard
+					image={nftObject[index]["images"]["image_small"]}
+					text="Estimated Price: "
+					value={formatter.format(
+						nftObject[index]["floor_adjusted_predicted_price"]
+					)}
+					key={index}
+				/>
+			);
+		}
+		return flufs;
+	};
 
-  return (
-    <>
-      {
-        isLoading ? (
-          <div className="w-full pt-16">
-            <Loader />
-            <p className="w-full text-center p-5">Loading ...</p>
-          </div>
-        ) : (
-          <>
-            {filteredItem && checked && filteredItem.length > 0 ? (
-              <div className="grid lg:grid-cols-3 xl:grid-cols-4 gap-5 w-full justify-items-center">
-                {datafiltered()}
-              </div>
-            ) : (
-              <div className="grid lg:grid-cols-3 xl:grid-cols-4 gap-5 justify-items-center">
-                {dataFluf()}
-              </div>
-            )}
-            { (
-              <Pagination
-                pageLenght={pageLenght}
-                controlPageIndex={controlPageIndex + 1}
-                setControlPageIndex={setControlPageIndex}
-              />
-            )}
-          </>
-        )
-      }
-    </>
-  )
+	const datafiltered = () => {
+		const flufs: React.ReactElement[] = [];
+		for (
+			let index: number = controlPageIndex * 10;
+			index < controlPageIndex * 10 + 20;
+			index++
+		) {
+			if (!filteredItem[index]) return flufs;
+			flufs.push(
+				<NftCard
+					image={filteredItem[index]["images"]["image_small"]}
+					text="Estimated Price: "
+					value={formatter.format(
+						filteredItem[index]["floor_adjusted_predicted_price"]
+					)}
+					key={index}
+				/>
+			);
+		}
+		return flufs;
+	};
+
+	return (
+		<>
+			{isLoading ? (
+				<div className="w-full pt-16">
+					<Loader />
+					<p className="w-full text-center p-5">Loading ...</p>
+				</div>
+			) : (
+				<>
+					{filteredItem && checked && filteredItem.length > 0 ? (
+						<div className="grid lg:grid-cols-3 xl:grid-cols-4 gap-5 w-full justify-items-center">
+							{datafiltered()}
+						</div>
+					) : (
+						<div className="grid lg:grid-cols-3 xl:grid-cols-4 gap-5 justify-items-center">
+							{dataFluf()}
+						</div>
+					)}
+					{pageLenght > 1 ? (
+						<Pagination
+							pageLenght={pageLenght}
+							controlPageIndex={controlPageIndex + 1}
+							setControlPageIndex={setControlPageIndex}
+						/>
+					) : (
+						<></>
+					)}
+				</>
+			)}
+		</>
+	);
 }
---
+++ components/nftValuation/FilterColumn.tsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { Fade } from "react-awesome-reveal";
+
+// Components
+import ColumnOptionButton from "./nftFilterColumn/ColumnOptionButton";
+import FilterCheckBox from "./nftFilterColumn/FilterCheckBox";
+import FilterSelectorTraits from "./nftFilterColumn/FilterSelectorTraits";
+import FilterPrice from "./nftFilterColumn/FilterPrice";
+
+// Libraries
+import {
+	Status,
+	Currencies,
+	Filters,
+} from "../../lib/nftValuation/nftCommonTypes";
+import { typedKeys } from "../../lib/utilities";
+
+interface nftObject {
+	tokenId: string;
+	floor_adjusted_predicted_price: number;
+	traits: {
+		traitType: string;
+		value: string;
+	}[];
+	images: {
+		image_small: string;
+	};
+}
+
+interface IFilterColumn {
+	nftObject: nftObject[];
+	setfilteredItem: Function;
+	setChecked: Function;
+}
+
+export default function FilterColumn({
+	nftObject,
+	setfilteredItem,
+	setChecked,
+}: IFilterColumn) {
+	const [currency, setCurrency] = useState<Currencies>("eth");
+
+	const filterOptions = {
+		Status: {
+			name: "Status",
+			description: "",
+			children: (
+				<Fade duration={500} direction="down">
+					<FilterCheckBox
+						optionslist={Status}
+						filterObject={nftObject}
+						setfilteredItem={setfilteredItem}
+						setChecked={setChecked}
+					/>
+				</Fade>
+			),
+		},
+		Price: {
+			name: "Price Estimation",
+			description: "",
+			children: (
+				<Fade duration={500} direction="down">
+					<FilterPrice currency={currency} setCurrency={setCurrency} />
+				</Fade>
+			),
+		},
+		TraitFilter: {
+			name: "TraitFilter",
+			description: "",
+			children: (
+				<div className="flex flex-col p-1 rounded-3xl">
+					{typedKeys(Filters).map((filter) => {
+						return (
+							<Fade duration={500} direction="down" key={filter}>
+								<FilterSelectorTraits
+									title={filter}
+									setfilteredItem={setfilteredItem}
+									setChecked={setChecked}
+									filterObject={nftObject}
+								/>
+							</Fade>
+						);
+					})}
+				</div>
+			),
+		},
+	};
+
+	return (
+		<div className="flex flex-col p-2 nm-flat-medium rounded-3xl">
+			{typedKeys(filterOptions).map((filter) => (
+				<ColumnOptionButton title={filterOptions[filter].name} key={filter}>
+					{filterOptions[filter].children}
+				</ColumnOptionButton>
+			))}
+		</div>
+	);
+}
