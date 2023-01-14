4960c9cd05d91f4a33e73fdf9783a4b4a4fab4cc
GabCh15 <64225598+GabCh15@users.noreply.github.com>
2023-01-13 21:15:38 -0500

    Hotfix

--- components/InitWeb3Connect.tsx
+++ components/InitWeb3Connect.tsx
@@ -1,58 +1,47 @@
-import { useEffect, useState } from "react";
-import { FaWallet } from "react-icons/fa";
-import { useAccount } from "wagmi";
+import { useEffect, useState } from 'react'
+import { FaWallet } from 'react-icons/fa'
+import { useAccount } from 'wagmi'
 
 // web3auth service
-import web3authService from "../backend/services/Web3authService";
+import web3authService from '../backend/services/Web3authService'
 
 // Components
-import OvalButton from "./General/Buttons/OvalButton";
+import OvalButton from './General/Buttons/OvalButton'
 
 export default function InitWeb3Connect() {
-  const [userAddress, setUserAddress] = useState<string | undefined>(undefined)
-  const [isInit, setIsInit] = useState(false)
-  const {address} = useAccount()
-console.log(address)
-  useEffect(() => {
-    const init = async () => { setUserAddress(await web3authService.getAccounts()) }
-    init();
-  }, [])
+    const [isInit, setIsInit] = useState(false)
+    const { address } = useAccount()
 
-  const login = async () => {
-    await web3authService.connectWeb3Auth()
-    setUserAddress(await web3authService.getAccounts())
-  }
+    const login = async () => {
+        await web3authService.connectWeb3Auth()
+    }
 
-  const logout = async () => {
-    await web3authService.disconnectWeb3Auth()
-    setUserAddress(undefined)
-  }
+    const logout = async () => {
+        await web3authService.disconnectWeb3Auth()
+    }
 
-  useEffect(() => { setIsInit(true) }, [web3authService.getWeb3Auth])
-
-  return (
-    <>
-      {isInit && <>
-         {
-          userAddress
-            ? (
-              <div className="flex flex-row">
-                <OvalButton
-                  buttonFunction={() => logout()}
-                  label={`${address ? address : 'Loading ...'}`}
-                  backgroundColorClass={'bg-white'}
-                />
-              </div>
+    return (
+        <>
+            {address ? (
+                <div className="flex flex-row">
+                    <OvalButton
+                        buttonFunction={() => logout()}
+                        label={`${address ? address : 'Loading ...'}`}
+                        backgroundColorClass={'bg-white'}
+                    />
+                </div>
             ) : (
-              <OvalButton
-                buttonFunction={() => login()}
-                label={'User Connect'}
-                icon={<FaWallet className={`text-2xl z-10 text-grey-content pr-1 font-bold`} />}
-                backgroundColorClass={'bg-white'}
-              />
-            )
-        } 
-      </>}
-    </>
-  )
+                <OvalButton
+                    buttonFunction={() => login()}
+                    label={'User Connect'}
+                    icon={
+                        <FaWallet
+                            className={`text-2xl z-10 text-grey-content pr-1 font-bold`}
+                        />
+                    }
+                    backgroundColorClass={'bg-white'}
+                />
+            )}
+        </>
+    )
 }
