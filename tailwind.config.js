b164390831fa9e39b2c0b33b7e325348cb7485fc
Andres <andgonzalezcas@unal.edu.co>
2023-01-18 09:57:49 -0500

    hot grey fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -112,7 +112,6 @@ module.exports = {
           lightest: '#F8F9FD'
         },
         'background': '#111111',
-        'grey': '#292929',
         'white': '#ffffff',
         'tahiti': '#3ab7bf',
       },
