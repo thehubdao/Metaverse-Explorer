8f276160f3e1214a68167d8f382a4c6a24626f2b
Andres <andgonzalezcas@unal.edu.co>
2023-01-30 15:16:52 -0500

    change login label on connect button

--- components/ConnectButton.tsx
+++ components/ConnectButton.tsx
@@ -11,49 +11,45 @@ import web3authService from '../backend/services/Web3authService'
 import OvalButton from './General/Buttons/OvalButton'
 
 export default function ConnectButton() {
-    const { connect, connectors } = useConnect()
-    const { disconnect } = useDisconnect()
-    const { address } = useAccount()
-    const { data: signer } = useSigner()
+  const { connect, connectors } = useConnect()
+  const { disconnect } = useDisconnect()
+  const { address } = useAccount()
+  const { data: signer } = useSigner()
 
-    useEffect(() => {
-        if (!signer) return
-        const initAuth = async () => {
-            await web3authService.connectWeb3Auth(signer as Signer)
-            await initContract(signer as Signer)
-        }
-        initAuth()
-    }, [signer])
-
-    const login = async () => {
-        connect({ connector: connectors[0] })
+  useEffect(() => {
+    if (!signer) return
+    const initAuth = async () => {
+      await web3authService.connectWeb3Auth(signer as Signer)
+      await initContract(signer as Signer)
     }
-    const logout = async () => {
-        disconnect()
-    }
+    initAuth()
+  }, [signer])
 
-    return (
-        <>
-            {address ? (
-                <div className="flex flex-row">
-                    <OvalButton
-                        buttonFunction={() => logout()}
-                        label={`${address ? address : 'Loading ...'}`}
-                        backgroundColorClass={'bg-white'}
-                    />
-                </div>
-            ) : (
-                <OvalButton
-                    buttonFunction={() => login()}
-                    label={'User Connect'}
-                    icon={
-                        <FaWallet
-                            className={`text-2xl z-10 text-grey-content pr-1 font-bold`}
-                        />
-                    }
-                    backgroundColorClass={'bg-white'}
-                />
-            )}
-        </>
-    )
+  const login = async () => {
+    connect({ connector: connectors[0] })
+  }
+  const logout = async () => {
+    disconnect()
+  }
+
+  return (
+    <>
+      {address ? (
+        <div className="flex flex-row">
+          <OvalButton
+            buttonFunction={() => logout()}
+            label={`${address}`}
+            backgroundColorClass={'bg-white'}
+          />
+        </div>
+      ) : (
+        <OvalButton
+          buttonFunction={() => login()}
+          label={'Login'}
+          icon={<FaWallet className={`text-2xl z-10 text-grey-content pr-1 font-bold`} />}
+          backgroundColorClass={'bg-white'}
+        />
+      )}
+    </>
+  )
 }
