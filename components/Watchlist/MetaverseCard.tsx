9967cdcc93b52e76e67e5090210f0fccc5d0a0fb
Andres <andgonzalezcas@unal.edu.co>
2023-02-03 10:54:28 -0500

    added animation on selected metaverse watchlist

--- components/Watchlist/MetaverseCard.tsx
+++ components/Watchlist/MetaverseCard.tsx
@@ -21,9 +21,9 @@ const MetaverseCard = ({
         <button
             key={label}
             onClick={() => setMetaverse(metaverseKey)}
-            className={`flex flex-col items-center justify-center space-y-2 rounded-xl cursor-pointer p-2 px-3 pt-4 w-[200px] h-[250px] group focus:outline-none nm-flat-hard hover:scale-105 transition ease-in-out duration-300 ${currentMetaverse === label
+            className={`flex flex-col items-center justify-center space-y-2 rounded-xl cursor-pointer p-2 px-3 pt-4 w-[200px] h-[250px] group focus:outline-none nm-flat-hard transition ease-in-out duration-300 ${currentMetaverse === label
                     ? ' text-gray-200 nm-inset-hard'
-                    : ' hover:border-opacity-100 nm-flat-hard'
+                    : ' nm-flat-hard'
                 }`}
         >
             <Image src={imageUrl} width={100} height={100} />
