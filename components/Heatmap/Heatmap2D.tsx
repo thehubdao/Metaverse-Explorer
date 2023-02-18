463e0bc6efc162bbd7dc1d98f1f69c4d5803fd73
GabCh15 <gabch15@hotmail.com>
2023-02-18 11:25:28 -0500

    Add dead lands

--- components/Heatmap/Heatmap2D.tsx
+++ components/Heatmap/Heatmap2D.tsx
@@ -184,7 +184,45 @@ const Heatmap2D = ({
     )
     setIsLoading(true)
     socketService.startRender(metaverse)
-    socketService.onRenderFinish(() => { setIsLoading(false) })
+    socketService.onRenderFinish(async () => {
+      const localChunks = chunks
+      if (metaverse == "sandbox") for (let i = -204; i <= 203; i++) {
+        const x = i
+        for (let j = -203; j <= 204; j++) {
+          const y = j
+          if (!mapData[x + ',' + y]) {
+            const borderURL = 'images/full_border_dead.jpg'
+            const rectangle: any = new PIXI.Sprite(await PIXI.Texture.fromURL(borderURL, {
+              mipmap: PIXI.MIPMAP_MODES.ON,
+            }))
+            const chunkX = Math.floor(x / CHUNK_SIZE)
+            const chunkY = Math.floor(y / CHUNK_SIZE)
+            const chunkKey = `${chunkX}:${chunkY}`
+            let chunkContainer = localChunks[chunkKey]
+            rectangle.tint = 0x2d4162
+            rectangle.width = rectangle.height =
+              TILE_SIZE
+            rectangle.position.set(
+              x * TILE_SIZE - chunkX * BLOCK_SIZE,
+              y * TILE_SIZE - chunkY * BLOCK_SIZE
+            )
+            rectangle.type = 'dead'
+            if (!chunkContainer) {
+              chunkContainer = localChunks[chunkKey] = new Container()
+              chunkContainer.position.set(
+                chunkX * BLOCK_SIZE,
+                chunkY * BLOCK_SIZE
+              )
+              setChunks(localChunks)
+            }
+            chunkContainer.addChild(rectangle)
+            viewport.addChild(chunkContainer)
+          }
+        }
+      }
+
+      setIsLoading(false)
+    })
     return () => {
       socketService.disconnect()
     }
@@ -231,7 +269,7 @@ const Heatmap2D = ({
       const sandbox_bg_url = 'images/sandbox_bg.jpg'
       const texture = await PIXI.Texture.fromURL(sandbox_bg_url, {
       })
-      const mapBackground = new PIXI.Sprite(metaverse == 'sandbox' ? texture : PIXI.Texture.WHITE)
+      const mapBackground = new PIXI.Sprite(/* metaverse == 'sandbox' ? texture : */ PIXI.Texture.WHITE)
       mapBackground.position.set(-204 * TILE_SIZE, -203 * TILE_SIZE)
       mapBackground.width = 410 * TILE_SIZE
       mapBackground.height = 410 * TILE_SIZE
@@ -240,7 +278,7 @@ const Heatmap2D = ({
     }
 
     const getMetaverseData = async () => {
-      await setBackground()
+      /* await setBackground() */
       let dataCall: any = await fetch(
         process.env.SOCKET_SERVICE + `/limits?metaverse=${metaverse}`
       )
@@ -280,6 +318,7 @@ const Heatmap2D = ({
       const child = chunkContainer?.children.find(
         (child: any) => child.x === x && child.y === y
       )
+      if(child && child.type == 'dead') return
       if (child) {
         if (currentSprite) {
           currentSprite.tint = currentTint
@@ -338,6 +377,7 @@ const Heatmap2D = ({
       let lands = await setColours(mapData, globalFilter, metaverseData)
       for (const key in chunks) {
         for (const child of chunks[key].children) {
+          if(!lands[child.name]) continue
           let tile: any = filteredLayer(
             child.landX,
             child.landY,
