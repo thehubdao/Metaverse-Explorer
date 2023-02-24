7f36844f58e92c89cbef0d38e35de2f25f5154d3
Sebastian Ortiz <jsoc1999@gmail.com>
2023-02-24 01:47:31 -0500

    New Analytics view

--- components/Analytics/AnalyticsMvChoice.tsx
+++ components/Analytics/AnalyticsMvChoice.tsx
@@ -13,44 +13,40 @@ const AnalyticsMvChoice = ({ allMetaverse, setAllMetaverse }: Props) => {
     sandbox: { logo: '/images/the-sandbox-sand-logo.png' },
     decentraland: { logo: '/images/decentraland-mana-logo.png' },
     /* 'axie-infinity': { logo: '/images/axie-infinity-axs-logo.png' }, */
-    'somnium-space': { logo: '/images/somnium-space-cube-logo.webp'}
+    'somnium-space': { logo: '/images/somnium-space-cube-logo.webp' }
   }
   return (
     <div className='w-full h-full p-2'>
       {/* Metaverse Buttons */}
-      <div className='nm-insert-hard flex justify-center gap-5 sm:gap-8'>
+      <div className='nm-insert-hard flex justify-center gap-2 sm:gap-4'>
         {typedKeys(mvOptions).map((landKey: Metaverse) => (
           <button
             key={landKey}
-            onClick={() =>{
+            onClick={() => {
               let state: boolean;
-              allMetaverse[landKey].active ? state=false : state=true 
+              allMetaverse[landKey].active ? state = false : state = true
               setAllMetaverse((prevState: any) => ({
-              ...prevState,
+                ...prevState,
                 [landKey]: {
                   ...prevState[landKey],
                   active: state
                 },
               })
-            )}}
-            className={`flex flex-col items-center justify-center space-y-2 rounded-xl cursor-pointer p-2 px-3 pt-4 md:w-40 md:h-[12rem] w-30 h-32 group focus:outline-none ${
-              allMetaverse[landKey].active
-                ? 'border-opacity-20 nm-inset-medium'
-                : 'nm-flat-medium border-opacity-20 hover:border-opacity-100'
-            } border border-gray-400 transition duration-300 ease-in-out`}
+              )
+            }}
+            className={`flex flex-col items-center justify-center rounded-xl cursor-pointer p-1 w-10 h-10 group focus:outline-none ${allMetaverse[landKey].active
+              ? 'border-opacity-20 nm-inset-medium'
+              : 'nm-flat-medium border-opacity-20 hover:border-opacity-100'
+              } border border-gray-400 transition duration-300 ease-in-out`}
           >
             <OptimizedImage
               src={mvOptions[landKey].logo}
               height={60}
               width={60}
               objectFit='contain'
-              className={`w-10 ${
-                allMetaverse[landKey].active ? 'grayscale-0' : 'grayscale'
-              } group-hover:grayscale-0 transition duration-300 ease-in-out`}
+              className={` ${allMetaverse[landKey].active ? 'grayscale-0' : 'grayscale'
+                } group-hover:grayscale-0 transition duration-300 ease-in-out`}
             />
-            <p className='font-medium text-xs md:text-sm pt-1'>
-              {formatName(landKey)}
-            </p>
           </button>
         ))}
       </div>
