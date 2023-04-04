10243790ef41b20c6ac307337f1a6b2bddfd0736
Andres <andgonzalezcas@unal.edu.co>
2023-04-03 21:29:55 -0500

    Added the indicator of the number of lands in the shopping cart.

--- components/ShoppingCart.tsx
+++ components/ShoppingCart.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Alert, Snackbar } from '@mui/material'
 import Image from 'next/image'
+import { useSelector } from "react-redux";
 
 interface Props {
   setOpenShopCartModal: Function
@@ -9,14 +10,11 @@ interface Props {
 const ShoppingCart = ({ setOpenShopCartModal }: Props) => {
   const [numItems, setNumItems] = useState(0);
   const [openNotification, setOpenNotification] = useState(false);
+  const shopList = useSelector((state: any) => state.shopCartList)
 
   useEffect(() => {
-    // Fetch the number of items in the shopping cart from your API
-    fetch("/api/shopping-cart")
-      .then((response) => response.json())
-      .then((data) => setNumItems(data.numItems))
-      .catch((error) => console.error(error));
-  }, []);
+    setNumItems(shopList.length)
+  }, [shopList.length])
 
   function handleClick() {
     setOpenNotification(true)
@@ -29,9 +27,10 @@ const ShoppingCart = ({ setOpenShopCartModal }: Props) => {
 
   return (
     <>
-      <button onClick={handleClick} className='flex flex-col  mx-8 mt-6 items-center justify-center rounded-2xl cursor-pointer p-2 w-16 h-16 group focus:outline-none bg-[#F9FAFB] shadow-xl'>
+      <button onClick={handleClick} className='relative flex flex-col mx-8 mt-6 items-center justify-center rounded-2xl cursor-pointer p-2 w-16 h-16 group focus:outline-none bg-[#F9FAFB] shadow-xl'>
         <Image src={'/images/shopping-cart.svg'} width={30} height={26} alt="shopping cart" className='' />
         {/* {numItems} */}
+        <p className="absolute bg-[#1AB3F3] text-white text-sm font-bold px-2 rounded-md bottom-2 right-2">{numItems ? numItems : ''}</p>
       </button>
       <Snackbar
         open={openNotification}
