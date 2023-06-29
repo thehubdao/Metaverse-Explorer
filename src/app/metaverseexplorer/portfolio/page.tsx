87000b29a114f989cd18bb8e424c16986e14fe7a
smarquezmgh <santiago.marquez@thedac.info>
2023-06-29 15:48:35 -0500

    subheader added

--- src/app/metaverseexplorer/portfolio/page.tsx
+++ src/app/metaverseexplorer/portfolio/page.tsx
@@ -5,13 +5,13 @@ import IsLoginUI from "../../ui/isLogin.ui";
 export default function Portfolio (){
     const isConnected = false
     return (
-        <div className="px-16">
+        <>
         
             {!isConnected ?
             <IsLoginUI message="Please log in to show your portfolio"/>
             :
             <>
-                <div className="flex items-center justify-between p-8 space-x-20 mt-10">
+                <div className="flex items-center justify-between p-8 space-x-20 mt-10 px-20">
                     <div className="flex flex-col space-y-3 max-w-2xl text-nm-dm-icons">
                         <p className="text-2xl font-semibold">Description</p>
                         <p className="text-sm">The MGH LAND price estimator uses AI to calculate the fair value of LANDs and help you find undervalued ones.  Leverage our heatmap to quickly get an overview of the Sandbox Map and get insights about current price trends. The valuations are updated at a daily basis.</p>
@@ -24,7 +24,7 @@ export default function Portfolio (){
 
                         <div className="flex flex-col space-y-2 items-center shadow-relief-32 py-3 px-10 rounded-3xl bg-nm-fill">
                             <div className=" font-black text-2xl">
-                                {/* <PriceList predictions={totalWorth} /> */}
+                                
                             </div>
                             <p className="text-sm">Total Value worth</p>
                         </div>
@@ -33,6 +33,6 @@ export default function Portfolio (){
             </>
             }
             <FooterUI/>
-        </div>
+        </>
     )
 }
