6ec08a2a56180a35e967ba3e17da5ec8a7c5aba1
Alonso <cardenasjairods@gmail.com>
2023-06-30 09:36:03 -0500

    adding arcana connector

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,47 +1,74 @@
 import '@rainbow-me/rainbowkit/styles.css';
 
 import {
-    getDefaultWallets,
     RainbowKitProvider,
+    connectorsForWallets
 } from '@rainbow-me/rainbowkit';
+
+import { metaMaskWallet, walletConnectWallet } from "@rainbow-me/rainbowkit/wallets";
 
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 
 import {
     mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    zora,
+    polygon
 } from 'wagmi/chains';
+
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
+import { getArcanaAuthProvider } from './utils/getArcanaAuthProvider';
+
+import { ArcanaConnector } from '@arcana/auth-wagmi';
+
+const ArcanaRainbowConnector = () => {
+    return {
+        id: "arcana-auth",
+        name: "Arcana Wallet",
+        iconUrl: "https://gateway.arcana.network/api/v2/app/447/logo?type=light&orientation=horizontal",
+        iconBackground: "#101010",
+        createConnector: () => {
+            const connector = new ArcanaConnector({
+                options: {
+                    auth: getArcanaAuthProvider()
+                }
+            });
+            return {
+                connector,
+            }
+        }
+    };
+};
+
 const { chains, publicClient } = configureChains(
-    [mainnet],
+    [mainnet, polygon],
     [
-        //alchemyProvider({ apiKey: process.env.ALCHEMY_ETHEREUM_KEY! }),
+        alchemyProvider({ apiKey: process.env.ALCHEMY_ETHEREUM_KEY! }),
         publicProvider()
     ]
 );
 
-const { connectors } = getDefaultWallets({
-    appName: 'My RainbowKit App',
-    projectId: process.env.WALLETCONNECT_PROJECT_ID!,
-    chains
-});
+const connectors = connectorsForWallets([
+    {
+        groupName: "Recommended",
+        wallets: [
+            ArcanaRainbowConnector() as any,
+            metaMaskWallet({ chains, projectId: process.env.WALLETCONNECT_PROJECT_ID! }),
+            walletConnectWallet({ chains, projectId: process.env.WALLETCONNECT_PROJECT_ID! })
+        ],
+    },
+]);
 
 const wagmiConfig = createConfig({
-    autoConnect: true,
     connectors,
-    publicClient
+    publicClient,
 })
 
-export function Providers({ children }) {
+export function Providers({ children }: any) {
     return (
         <WagmiConfig config={wagmiConfig}>
             <RainbowKitProvider chains={chains}>
-                { children }
+                {children}
             </RainbowKitProvider>
         </WagmiConfig>
     );
