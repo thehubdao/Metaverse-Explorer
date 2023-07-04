fa29af5a81b08b351ab8d3631434cd0046cb8ef1
OssFate <mantis1512@gmail.com>
2023-07-03 21:31:42 -0500

    Added logging util with logErrorServer function Currently only logs on console, the idea is always try to use this function and at some point add a better logging

---
+++ src/server/enums/logging.enum.ts
@@ -0,0 +1,7 @@
+﻿export const enum ServerModule {
+  ExampleApi = 'ExampleApiModule',
+}
+
+export const enum DefErrorMsg {
+  ErrorGettingBody= 'Error getting body from request!',
+}
---
+++ src/server/utils/logging.util.ts
@@ -0,0 +1,5 @@
+﻿import {DefErrorMsg, ServerModule} from "../enums/logging.enum";
+
+export async function LogErrorServer(module: ServerModule, msg: string | DefErrorMsg, err: unknown) {
+  console.error(module, '-', msg);
+}
