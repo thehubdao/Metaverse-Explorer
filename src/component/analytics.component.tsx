6ad16eb43d19ae827b3063a9e24cb0c57b913687
Andres <andgonzalezcas@unal.edu.co>
2023-07-27 11:25:02 -0500

    Split functional view component and UI component on analytics view

--- src/component/analytics.component.tsx
+++ src/component/analytics.component.tsx
@@ -1,34 +1,76 @@
-import ChartCurrencyFilterSelector from "../ui/analytics/chartCurrencyFilterSelector.ui";
-import ChartGridSelector from "../ui/analytics/chartGridSelector.ui";
-import ChartMetaverseFilterSelector from "../ui/analytics/chartMetaverseFilterSelector.ui";
-import ChartNameFilterSelector from "../ui/analytics/chartNameFilterSelector.ui";
-import ChartTimeFilterSelector from "../ui/analytics/chartTimeFilterSelector.ui";
-import LinearChartUI from "../ui/charts/linearChart.ui";
+'use client' // This is a client component 👈🏽
+
+import { useEffect, useState } from "react";
+import { CHART_ROUTES, generateTestApiData } from "../utils/analyticsChart";
+
 import FooterUI from "../ui/footer.ui";
+import AnalyticsUI from "../ui/analytics/analytics.ui";
+import { AnalyticChartData } from "../interfaces/charts";
 
 export default function AnalyticsComponent() {
-    return (
-        <>
-            <div className="w-full px-16 mt-16 text-nm-dm-icons font-bold text-sm">
-                <div className="h-12 flex justify-between items-center">
-                    <div className="flex gap-6 ">
-                        <ChartNameFilterSelector />
-                        <ChartMetaverseFilterSelector />
-                    </div>
-                    <div className="flex gap-6">
-                        <ChartCurrencyFilterSelector />
-                        <ChartTimeFilterSelector />
-                        <ChartGridSelector />
-                    </div>
-                </div>
-                <div className="grid grid-cols-2 gap-5 my-6">
-                    {Array.from({ length: 10 }, (_, index) => (
-                        <LinearChartUI />
-                    ))}
-                </div>
+  // State to hold the fetched analytics data for different charts
+  const [analyticsData, setAnalyticsData] = useState<AnalyticChartData[]>([]);
 
-            </div>
-            <FooterUI />
-        </>
-    )
+  useEffect(() => {
+    // Function to fetch data from APIs and update the state
+    const fetch = async () => {
+      // TODO: The generateTestApiData function should be changed to one that brings real data from the api.
+      const DataToBeReturned: AnalyticChartData[] = await Promise.all(
+        // Fetch data for each chart using map and Promise.all
+        CHART_ROUTES.map(async (chart) => {
+          // Fetch data for sandbox platform
+          const sandboxFetch = await generateTestApiData(1000, chart[1].route).then((API_DATA) => {
+            return { API_DATA, status: 'ready' }
+          }).catch(error => {
+            return { API_DATA: [], status: 'error' }
+          })
+
+          // Fetch data for decentraland platform
+          const decentralandFetch = await generateTestApiData(1000, chart[1].route).then((API_DATA) => {
+            return { API_DATA, status: 'ready' }
+          }).catch(error => {
+            return { API_DATA: [], status: 'error' }
+          })
+
+          // Fetch data for somnium space platform
+          const somniumSpaceFetch = await generateTestApiData(1000, chart[1].route).then((API_DATA) => {
+            return { API_DATA, status: 'ready' }
+          }).catch(error => {
+            return { API_DATA: [], status: 'error' }
+          })
+
+          // Combine data for all platforms into a single object
+          const data = {
+            'sandbox': sandboxFetch.API_DATA,
+            'decentraland': decentralandFetch.API_DATA,
+            'somnium-space': somniumSpaceFetch.API_DATA
+          }
+
+          // Return the aggregated data for the chart
+          return {
+            chartEnum: chart[0],
+            route: chart[1].route,
+            label: chart[1].label,
+            description: chart[1].description,
+            data,
+            status: [sandboxFetch.status, decentralandFetch.status, somniumSpaceFetch.status].includes('error') ? 'error' : 'ready'
+          }
+        })
+      )
+      // Update the state with the fetched data
+      setAnalyticsData(DataToBeReturned)
+    }
+
+    // Fetch the data when the component mounts
+    fetch();
+  }, [])
+
+  return (
+    <>
+      {/* Render the AnalyticsUI component with the fetched analyticsData */}
+      <AnalyticsUI analyticsData={analyticsData} />
+      {/* Render the FooterUI component */}
+      <FooterUI />
+    </>
+  )
 }
---
+++ src/ui/analytics/analytics.ui.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import ChartCurrencyFilterSelectorUI from "./chart/chartCurrencyFilterSelector.ui";
+import ChartGridSelectorUI from "./chart/chartGridSelector.ui";
+import ChartMetaverseFilterSelectorUI from "./chart/chartMetaverseFilterSelector.ui";
+import ChartNameFilterSelectorUI from "./chart/chartNameFilterSelector.ui";
+import ChartTimeFilterSelectorUI from "./chart/chartTimeFilterSelector.ui";
+import LinearChartUI from "./chart/linearChart.ui";
+import { Currencies, Metaverses } from "../../enums/common";
+import { AnalyticChartData } from "../../interfaces/charts";
+import { AnalyticsChartRoutes } from "../../enums/charts";
+
+// Interface for props used in the AnalyticsUI component.
+interface AnalyticsUIProps {
+  analyticsData: AnalyticChartData[]; // Array of analytic chart data to be displayed
+}
+
+//* Main component for the Analytics UI.
+export default function AnalyticsUI({ analyticsData }: AnalyticsUIProps) {
+  // State variables to manage the UI filters and settings
+  const [gridChartCols, setGridChartCols] = useState<'col-span-1' | 'col-span-2'>('col-span-1');
+  const [visibleDays, setVisibleDays] = useState<7 | 30 | 60 | 183 | 365>(7);
+  const [visibleMetaverses, setVisibleMetaverses] = useState<Metaverses[]>([]);
+  const [visibleCharts, setVisibleCharts] = useState<AnalyticsChartRoutes[]>([]);
+  const [currencySelected, setCurrencySelected] = useState<Currencies>(Currencies.Ethereum);
+
+  return (
+    <div className="w-full px-16 mt-16 text-nm-dm-icons font-bold text-sm">
+      <div className="h-12 flex justify-between items-center">
+        <div className="flex gap-6 ">
+          {/* Component for chart name filter selector */}
+          <ChartNameFilterSelectorUI visibleCharts={visibleCharts} setVisibleCharts={setVisibleCharts} />
+          {/* Component for chart metaverse filter selector */}
+          <ChartMetaverseFilterSelectorUI visibleMetaverses={visibleMetaverses} setVisibleMetaverses={setVisibleMetaverses} />
+        </div>
+        <div className="flex gap-6">
+          {/* Component for chart currency filter selector */}
+          <ChartCurrencyFilterSelectorUI currencySelected={currencySelected} setCurrencySelected={setCurrencySelected} />
+          {/* Component for chart time filter selector */}
+          <ChartTimeFilterSelectorUI visibleDays={visibleDays} setVisibleDays={setVisibleDays} />
+          {/* Component for chart grid selector */}
+          <ChartGridSelectorUI setGridChartCols={setGridChartCols} gridChartCols={gridChartCols} />
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-5 my-6">
+        {/* Display a message while the chart data is loading */}
+        {analyticsData.length === 0 && <p>loading charts...</p>}
+        {/* Render each chart based on the selected filters */}
+        {analyticsData.map((chart, index) => {
+          return (visibleCharts.length === 0 || visibleCharts.includes(chart.chartEnum)) && (
+            <div key={index} className={`w-full h-full ${gridChartCols}`}>
+              {/* Component for the linear chart */}
+              <LinearChartUI chartApiData={chart} chartOptions={{ cols: gridChartCols, visibleDays, visibleMetaverses, currencySelected }} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
