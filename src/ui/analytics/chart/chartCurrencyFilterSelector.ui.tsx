f21e0d86c3b94e9cb78e8161eb4d4f3b60f55f5c
Andres <andgonzalezcas@unal.edu.co>
2023-07-27 11:16:50 -0500

    Updated and moved to a more appropriate folder for the chartCurrencyFilterSelect component UI.

---
+++ src/ui/analytics/chart/chartCurrencyFilterSelector.ui.tsx
@@ -0,0 +1,29 @@
+import { Currencies } from "../../../enums/common";
+import { CURRENCY_FILTERS } from "../../../utils/analyticsChart";
+
+// Interface for props used in the component.
+interface ChartCurrencyFilterSelectorUI {
+  currencySelected: Currencies; // Currently selected currency
+  setCurrencySelected: React.Dispatch<React.SetStateAction<Currencies>>; // Function to set the selected currency
+}
+
+//* React functional component responsible for rendering the currency filter selector UI.
+export default function ChartCurrencyFilterSelectorUI({ currencySelected, setCurrencySelected }: ChartCurrencyFilterSelectorUI) {
+  return (
+    // Container for the currency filter selector buttons.
+    <div className="flex gap-3">
+      {/* Mapping through the available currency filters */}
+      {CURRENCY_FILTERS.map(currency => {
+        return (
+          <button
+            key={currency.currency} // React key to identify each button uniquely
+            className={`rounded-lg h-12 bg-nm-fill px-2 ${currencySelected === currency.currency ? 'shadow-inset' : 'shadow-relief-12'}`}
+            onClick={() => { setCurrencySelected(currency.currency) }}
+          >
+            {currency.name}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
