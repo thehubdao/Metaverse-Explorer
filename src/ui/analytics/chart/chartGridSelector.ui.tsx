c005c46bb7176dabac40a22098598ff06f3a5249
Andres <andgonzalezcas@unal.edu.co>
2023-08-04 16:34:13 -0500

    remove chart grid selector ui unnecesary commits

--- src/ui/analytics/chart/chartGridSelector.ui.tsx
+++ src/ui/analytics/chart/chartGridSelector.ui.tsx
@@ -1,15 +1,12 @@
 import Image from "next/image";
 
-// Interface for props used in the component.
 interface ChartGridSelectorUIProps {
   setGridChartCols: React.Dispatch<React.SetStateAction<'col-span-1' | 'col-span-2'>>;
   gridChartCols: 'col-span-1' | 'col-span-2';
 }
 
-//* React functional component responsible for rendering the chart grid selector UI.
 export default function ChartGridSelectorUI({ gridChartCols, setGridChartCols }: ChartGridSelectorUIProps) {
   return (
-    // Container for the chart grid selector buttons.
     <div className="flex justify-center items-center gap-3">
       {/* Button to change the chart grid to 1 column */}
       <button
@@ -18,10 +15,10 @@ export default function ChartGridSelectorUI({ gridChartCols, setGridChartCols }:
         ${(gridChartCols === 'col-span-1') ? 'shadow-inset' : 'shadow-relief-12'}`}
       >
         <Image
-          src='/images/icons/chart/ic_two-col.svg' // Path to the image file
-          width={22} // Width of the image
-          height={22} // Height of the image
-          alt="button to change charts grid to 2 cols" // Alternate text for accessibility
+          src='/images/icons/chart/ic_two-col.svg'
+          width={22}
+          height={22}
+          alt="button to change charts grid to 2 cols"
         />
       </button>
       {/* Button to change the chart grid to 2 columns */}
@@ -31,10 +28,10 @@ export default function ChartGridSelectorUI({ gridChartCols, setGridChartCols }:
         ${(gridChartCols === 'col-span-2') ? 'shadow-hollow-8' : 'shadow-relief-12'}`}
       >
         <Image
-          src='/images/icons/chart/ic_one-col.svg' // Path to the image file
-          width={22} // Width of the image
-          height={22} // Height of the image
-          alt="button to change charts grid to 1 col" // Alternate text for accessibility
+          src='/images/icons/chart/ic_one-col.svg'
+          width={22}
+          height={22}
+          alt="button to change charts grid to 1 col"
         />
       </button>
     </div>
