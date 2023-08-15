9faaedcb998c28a4022b028d649c122bafe27cb9
OswaldUnity <oswald@thedac.info>
2023-08-15 14:52:56 -0500

    Added shared type for lands depending on metaverse Added guards for this types

--- src/interfaces/land.interface.ts
+++ src/interfaces/land.interface.ts
@@ -1,41 +1,46 @@
-﻿/***
- * For Clicks and Searches
- */
-export interface LandCoords {
-  x?: string | number;
-  y?: string | number;
-}
-
-export interface Coords {
+﻿export interface Coords {
   x: number | undefined;
   y: number | undefined;
 }
 
 export interface DecentralandTile {
-  type: number | undefined;
-  top: string | undefined;
-  left: string | undefined;
-  topLeft: string | undefined;
+  type: number;
+  top: boolean;
+  left: boolean;
+  topLeft: boolean;
 }
 
 export interface LandData {
-  watchlist?: boolean;
-  percent?: number;
-  center?: LandCoords;
-  coords?: Coords;
+  keyIndex: number;
+  tokenId: string;
+  current_price_eth: number;
   eth_predicted_price: number;
   floor_adjusted_predicted_price: number;
-  tokenId: string;
   history_amount: number;
   max_history_price: number;
-  current_price_eth: number;
-  land_type?: number;
-  geometry?: LandCoords[];
-  tile?: DecentralandTile;
+  coords: Coords;
+  
+  watchlist?: boolean;
+  percent?: number;
   price_difference?: number;
   transfers?: number;
   basic?: number;
   listed_lands?: number;
   last_month_sells?: number;
   portfolio?: number;
+}
+
+export interface LandDecentraland extends LandData {
+  isDcl: boolean;
+  tile: DecentralandTile;
+}
+
+export interface LandSandbox extends LandData {
+  isSandbox: boolean;
+  land_type?: number;
+}
+
+export interface LandSomniumSpace extends LandData {
+  isSSpace: boolean;
+  geometry: Coords[];
 }
---
+++ src/utils/heatmap/land.util.ts
@@ -0,0 +1,10 @@
+﻿import {LandType} from "../../types/heatmap/land.type";
+import {LandDecentraland, LandSandbox} from "../../interfaces/land.interface";
+
+export function IsLandDecentraland(land: LandType): land is LandDecentraland {
+  return (land as LandDecentraland).isDcl;
+}
+
+export function IsLandSandBox(land: LandType): land is LandSandbox {
+  return (land as LandSandbox).isSandbox;
+}
