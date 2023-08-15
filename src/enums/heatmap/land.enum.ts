4fc702cb9f45f6a2cb6fbb6d0f1d507d7630c41b
OswaldUnity <oswald@thedac.info>
2023-08-15 14:15:30 -0500

    Added enum for land border texture

---
+++ src/enums/heatmap/land.enum.ts
@@ -0,0 +1,7 @@
+﻿export const enum LandBorderTexture {
+  FullBorder = 'full_border.png',
+  TopBorder = 'top_border.png',
+  LeftBorder = 'left_border.png',
+  TopLeftBorder = 'topLeft_border.png',
+  FillBorder = 'fill_border.png',
+}
