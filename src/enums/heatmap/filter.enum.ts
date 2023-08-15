031b324bb6eb7d729dda2a10164b7f313da13b9a
OswaldUnity <oswald@thedac.info>
2023-08-15 14:57:26 -0500

    Added filter enum Can be better

---
+++ src/enums/heatmap/filter.enum.ts
@@ -0,0 +1,24 @@
+﻿export const enum LegendFilter {
+  Portfolio = 'portfolio',
+  Watchlist = 'watchlist',
+  OnSale = 'on-sale',
+  PremiumLands = 'premium-lands',
+}
+
+export const enum MapFilter {
+  eth_predicted_price,
+  price_difference,
+  transfers,
+  basic,
+  listed_lands,
+  floor_adjusted_predicted_price,
+  last_month_sells,
+  // Not using this filters for now..will delete if permanent
+  // variation_last_week,
+  // variation_last_four_weeks,
+  // variation_last_six_months
+}
+
+export const enum FilterPredictedPrice {
+  
+}
