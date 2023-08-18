f2dd7ae74bb9a7ded77a6f18b2579e15ad89a4ed
smarquezmgh <santiago.marquez@thedac.info>
2023-08-18 11:03:20 -0500

    login redux added in portfolio component

--- src/component/portfolio/portfolio.component.tsx
+++ src/component/portfolio/portfolio.component.tsx
@@ -1,7 +1,19 @@
+"use client"
+
+import { useAppSelector } from "../../state/hooks";
+import IsLoginUI from "../../ui/common/isLogin.ui";
 import PortfolioUI from "../../ui/portfolio/portfolio.ui";
 
 export default function PortfolioComponent() {
+  const isConnected = useAppSelector(state => state.login.connected)
+
   return (
-    <PortfolioUI/>
+    <>
+      {!isConnected ?
+        <IsLoginUI message="Please log in to show your portfolio" />
+        :
+        <PortfolioUI />
+      }
+    </>
   )
 }
