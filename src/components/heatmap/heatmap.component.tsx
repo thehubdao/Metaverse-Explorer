9eea5ca0e44ba5e5f0dbf18aaba9b566408ee13a
OswaldUnity <oswald@thedac.info>
2023-08-30 16:20:48 -0500

    Fixed interval loading message

--- src/components/heatmap/heatmap.component.tsx
+++ src/components/heatmap/heatmap.component.tsx
@@ -48,8 +48,8 @@ interface Heatmap2DProps {
   y: number | undefined;
   
   filter?: MapFilter;
-  percentFilter?: PercentFilter;
   legendFilter?: LegendFilter;
+  percentFilter?: PercentFilter;
   
   onClickLand: (landRawData: unknown) => void;
   initialX: number;
@@ -73,9 +73,9 @@ export default function Heatmap2D({
                                   }: Heatmap2DProps) {
   
   const mapDivRef = useRef<HTMLDivElement>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [indexLoading, setIndexLoading] = useState<number>(RandomIntMax(LOAD_PHRASES_LENGHT));
+  
   const [mapLoadingState, setMapLoadingState] = useState<boolean>(false);
   
   
@@ -86,7 +86,21 @@ export default function Heatmap2D({
   // const portfolioLands = useAppSelector((state) => state.portfolio.list)
   
   useEffect(() => {
-    // setIsLoading(true);
+    console.warn('Set interval function');
+    if (!isLoading) return;
+
+    const intervalFunction = setInterval(() => {
+      setIndexLoading((prevIndex) => (prevIndex + 1) % LOAD_PHRASES_LENGHT)
+    }, 8 * 1000); // X seg * 1000ms
+    
+    return () => {
+      clearInterval(intervalFunction)
+    };
+
+  }, [isLoading]);
+  
+  useEffect(() => {
+    setIsLoading(true);
     
     // Init pixi variables
     initPixiViews();
@@ -320,20 +334,6 @@ export default function Heatmap2D({
       RenderStart(metaverse, 0);
     });
   }
-  
-  // useEffect(() => {
-  //   console.log('Set interval function');
-  //   if (!isLoading) return;
-  //
-  //   const intervalFunction = setInterval(() => {
-  //     setIndexLoading((prevIndex) => (prevIndex + 1) % LOAD_PHRASES_LENGHT)
-  //   }, 8 * 1000); // X seg * 1000ms
-  //
-  //   return () => {
-  //     clearInterval(intervalFunction)
-  //   };
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
 
   // Filter update
   // useEffect(() => {
@@ -344,84 +344,6 @@ export default function Heatmap2D({
   //   globalLegendFilter = legendFilter;
   // }, [filter, percentFilter, legendFilter]);
   
-  // async function renderHandler([land, landKeyIndex]: string[]) {
-  //   if (viewport == undefined)
-  //     return LogError(Module.Heatmap, "Missing viewport while trying to render land!");
-  //  
-  //   const landData = FormatLand(land, metaverse);
-  //   if (landData.coords == undefined || landData.coords.x == undefined || landData.coords.y == undefined)
-  //     return LogError(Module.Heatmap, "Missing coordinates to render!");
-  //  
-  //   _landIndex = Number(landKeyIndex);
-  //   const localChunks: typeof chunks = {...chunks};
-  //   let name = '';
-  //  
-  //   // if (landData.coords?.y != undefined)
-  //     landData.coords.y *= -1;
-  //
-  //   if (address != undefined) {
-  //     //if (portfolioLands[metaverse as keyof typeof portfolioLands][land.tokenId]) land.portfolio = true
-  //     const wMRef = wList[metaverse];
-  //     if (wMRef == undefined || wMRef[landData.tokenId] == undefined) landData.watchlist = true;
-  //   }
-  //  
-  //   name = `${landData.coords.x},${landData.coords.y}`;
-  //  
-  //   _mapData[name] = landData;
-  //  
-  //   const tile = await FilteredLayer(
-  //     landData.coords?.x,
-  //     landData.coords?.y,
-  //     globalFilter,
-  //     globalPercentFilter,
-  //     globalLegendFilter,
-  //     landData
-  //   );
-  //  
-  //   const {color} = tile;
-  //   // eslint-disable-next-line no-console
-  //   console.log("RenderHandler Color: ", color);
-  //
-  //   // color = color.includes('rgb')
-  //   //   ? ColorRgbToHex(color.split('(')[1].split(')')[0])
-  //   //   : '0x' + color.split('#')[1];
-  //
-  //   const border = GetBorder(landData);
-  //   const border_url = `images/${border ?? ''}`;
-  //   const texture = border != undefined
-  //     ? await Texture.fromURL(border_url, { mipmap: MIPMAP_MODES.ON })
-  //     : Texture.WHITE;
-  //  
-  //   const rectangle: OtherSprite = new Sprite(texture);
-  //   const chunkX = Math.floor(landData.coords.x / CHUNK_SIZE);
-  //   const chunkY = Math.floor(landData.coords.y / CHUNK_SIZE);
-  //   const chunkKey = `${chunkX}:${chunkY}`;
-  //   let chunkContainer = localChunks[chunkKey];
-  //   rectangle.tint = color;
-  //   rectangle.width = rectangle.height = new Set([5, 6, 7, 8, 12]).has(landData.tile?.type ?? 5)
-  //     ? TILE_SIZE
-  //     : TILE_SIZE - BOUND_SIZE;
-  //   rectangle.name = `${landData.coords.x},${landData.coords.y}`;
-  //   rectangle.landX = landData.coords.x;
-  //   rectangle.landY = landData.coords.y;
-  //   rectangle.tokenId = landData.tokenId;
-  //   rectangle.position.set(
-  //     landData.coords.x * TILE_SIZE - chunkX * BLOCK_SIZE,
-  //     landData.coords.y * TILE_SIZE - chunkY * BLOCK_SIZE
-  //   );
-  //  
-  //   if (!chunkContainer) {
-  //     chunkContainer = localChunks[chunkKey] = new Container();
-  //     chunkContainer.position.set(
-  //       chunkX * BLOCK_SIZE,
-  //       chunkY * BLOCK_SIZE
-  //     );
-  //     setChunks(localChunks);
-  //   }
-  //   chunkContainer.addChild(rectangle);
-  //   viewport.addChild(chunkContainer);
-  // }
-  //
   // function onMouseMove(event: FederatedPointerEvent, currentSprite: OtherSprite | undefined, currentTint: string | undefined) {
   //   if (mapLoadingState) return;
   //   if (viewport == undefined) return void LogError(Module.Heatmap, "Missing viewport!");
@@ -474,159 +396,6 @@ export default function Heatmap2D({
   //     tint: newCurrentTint
   //   }
   // }
-  //
-  // useEffect(() => {
-  //   console.log('Init socket');
-  //   if (viewport == undefined)
-  //     return LogError(Module.Heatmap, "Missing viewport!");
-  //
-  //   // eslint-disable-next-line no-console
-  //   console.log('Creando socket', new Date().toISOString());
-  //
-  //   const socketServiceUrl = process.env.SOCKET_SERVICE;
-  //   if (socketServiceUrl == undefined)
-  //     return LogError(Module.Heatmap, "Missing SocketService env value!");
-  //
-  //   const tempLands: string[][] = [];
-  //   socketService = GetSocketService(
-  //     socketServiceUrl,
-  //     () => {
-  //       // eslint-disable-next-line no-console
-  //       console.log('Connected', new Date().toISOString());
-  //       socketService.renderStart(metaverse, _landIndex);
-  //     },
-  //     (landRawData: string[]) => {
-  //       // eslint-disable-next-line no-console
-  //       // console.log('Land Data: ', landRawData);
-  //       tempLands.push(landRawData);
-  //     }
-  //   );
-  //
-  //   setIsLoading(true);
-  //
-  //   socketService.onGiveLand(onClickLand);
-  //   socketService.onRenderFinish(async () => {
-  //     try {
-  //     for (const land of tempLands) {
-  //       console.log(land);
-  //       await renderHandler(land)
-  //     }
-  //     const localChunks = chunks
-  //     if (metaverse == "sandbox") for (let i = -204; i <= 203; i++) {
-  //       const x = i
-  //       for (let j = -203; j <= 204; j++) {
-  //         const y = j
-  //         if (!_mapData[`${x},${y}`]) {
-  //           const borderURL = 'images/full_border_dead.jpg'
-  //           const rectangle: OtherSprite = new Sprite(await Texture.fromURL(borderURL, {
-  //             mipmap: MIPMAP_MODES.ON,
-  //           }));
-  //           const chunkX = Math.floor(x / CHUNK_SIZE)
-  //           const chunkY = Math.floor(y / CHUNK_SIZE)
-  //           const chunkKey = `${chunkX}:${chunkY}`
-  //           let chunkContainer = localChunks[chunkKey]
-  //           //! SANDBOX DEAD COLOR
-  //           rectangle.tint = '#2d4162';
-  //           rectangle.width = rectangle.height =
-  //             TILE_SIZE
-  //           rectangle.position.set(
-  //             x * TILE_SIZE - chunkX * BLOCK_SIZE,
-  //             y * TILE_SIZE - chunkY * BLOCK_SIZE
-  //           )
-  //           rectangle.type = 'dead'
-  //           if (!chunkContainer) {
-  //             chunkContainer = localChunks[chunkKey] = new Container()
-  //             chunkContainer.position.set(
-  //               chunkX * BLOCK_SIZE,
-  //               chunkY * BLOCK_SIZE
-  //             )
-  //             setChunks(localChunks)
-  //           }
-  //           chunkContainer.addChild(rectangle)
-  //           viewport.addChild(chunkContainer)
-  //         }
-  //       }
-  //     }
-  //     }
-  //     catch (e) {
-  //       console.error(e);
-  //     }
-  //
-  //     setIsLoading(false)
-  //     setMapLoadingState(false)
-  //   })
-  //  
-  //   return () => {
-  //     socketService.disconnect()
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [viewport])
-  //
-  // useEffect(() => {
-  //   console.log('Some pixi');
-  //   const mapDivRefCurrent = mapDivRef.current;
-  //   if (mapDivRefCurrent == null) return;
-  //
-  //   _landIndex = 0;
-  //   setMap(undefined);
-  //   setViewport(undefined);
-  //   setChunks({});
-  //   // mapData = {}
-  //
-  //   const mapApp = new Application<HTMLCanvasElement>({
-  //     width: viewportWidth,
-  //     height: viewportHeight,
-  //     resolution: 1,
-  //     backgroundAlpha: 0,
-  //   });
-  //
-  //   // TODO Check how to apply this
-  //   // map.view.style.borderRadius = '24px'
-  //
-  //   // map.renderer.plugins.
-  //
-  //   const viewport = new Viewport({
-  //     screenWidth: viewportWidth,
-  //     screenHeight: viewportHeight,
-  //     passiveWheel: false,
-  //     events: mapApp.renderer.events
-  //     // screenWidth: viewportWidth,
-  //     // screenHeight: viewportHeight,
-  //     // interaction: map.renderer.plugins.interaction,
-  //     // passiveWheel: false,
-  //   })
-  //     .drag()
-  //     .pinch()
-  //     .wheel()
-  //     .clampZoom({
-  //       minWidth: TILE_SIZE * 8,
-  //       minHeight: TILE_SIZE * 8,
-  //       maxWidth: TILE_SIZE * 800,
-  //       maxHeight: TILE_SIZE * 800,
-  //     })
-  //     .zoom(TILE_SIZE * 200)
-  //
-  //   mapApp.stage.addChild(viewport)
-  //
-  //   // document.getElementById('map')?.appendChild(map.view)
-  //   mapDivRefCurrent.appendChild(mapApp.view);
-  //   setMap(mapApp);
-  //   setViewport(viewport)
-  //   viewport.moveCenter(initialX * TILE_SIZE, initialY * TILE_SIZE)
-  //
-  //   return () => {
-  //     try {
-  //       if (mapDivRefCurrent != null)
-  //         mapDivRefCurrent.removeChild(mapApp.view);
-  //
-  //       mapApp.destroy();
-  //       viewport.destroy();
-  //     } catch (e) {
-  //       LogError(Module.Heatmap, "Error disposing pixi items from view", e);
-  //     }
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [metaverse]);
   //
   // useEffect(() => {
   //   console.log('remove listeners');
