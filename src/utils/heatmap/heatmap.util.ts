63259656f9a8d3e3b19a5b6d1932d226e76ad833
OswaldUnity <oswald@thedac.info>
2023-08-31 23:25:28 -0500

    Removed useless heatmap util file

--- src/utils/heatmap/heatmap.util.ts
+++
@@ -1,6 +0,0 @@
-﻿import {DisplayObject} from "pixi.js";
-import {OtherSprite} from "../../interfaces/heatmap.interface";
-
-export function IsOtherSprite(obj: DisplayObject): obj is OtherSprite {
-  return 'type' in (obj as OtherSprite);
-}
