6db2c62fc01112a667603fa2858094996304fd67
OswaldUnity <oswald@thedac.info>
2023-08-31 23:38:07 -0500

    Removed useless heatmap interfaces

--- src/interfaces/heatmap.interface.ts
+++ src/interfaces/heatmap.interface.ts
@@ -28,89 +28,7 @@ export interface HotDealsCard {
   name: string;
 }
 
-export interface Coord {
-    x: number;
-    y: number;
-}
-
-export interface Atlas {
-    ITRM: Record<string, ValuationTile>;
-    decentraland?: Record<string, AtlasTile>;
-}
-
-export interface Tile {
-    color: string;
-    top?: boolean;
-    left?: boolean;
-    topLeft?: boolean;
-    scale?: number;
-}
-
-export interface AtlasTile {
-    x: number;
-    y: number;
-    type: number;
-    district_id?: number;
-    estate_id?: number;
-    left?: number;
-    top?: number;
-    name?: string;
-    topLeft?: number;
-    price?: number;
-    owner?: string;
-}
-
-interface ValuationTransfer {
-    timestamp: number;
-    time: string;
-    price: number;
-    priceUsd: number;
-    owner: string;
-    eth_price: number;
-}
-
-// This is how the End Valuation Tile looks, some information is added after the API Request
-export interface ValuationTile {
-    name: string;
-    predicted_price: number;
-    eth_predicted_price: number;
-    history?: ValuationTransfer[];
-    variation_last_week: number;
-    variation_last_four_weeks: number;
-    variation_last_six_months: number;
-    manipulation_index: number;
-    suggested_operation?: string;
-    coords: Coord;
-    center: Coord;
-    current_price_eth?: number;
-    best_offered_price_eth?: number;
-    percent?: number;
-    land_id?: string;
-    watchlist?: boolean;
-    portfolio?: boolean;
-    images: { image_url: string };
-    owner?: string;
-    external_link: string;
-    floor_adjusted_predicted_price?: number;
-    tokenId?: string;
-}
-
-export interface HeatmapSize {
-    maxX: number;
-    maxY: number;
-    minX: number;
-    minY: number;
-    initialY: number;
-    initialX: number;
-}
-
-export interface OtherSprite extends Sprite {
-    tokenId?: string;
-    landY?: number;
-    landX?: number;
-    type?: string;
-}
-
+// TODO: use a better name, this interface refers to the different information related to land sprites rendered on the heatmap
 export interface LandRectangle {
     name: string;
     tokenId: string;
@@ -118,7 +36,5 @@ export interface LandRectangle {
     landX: number;
     land: LandType;
     spriteRef: Sprite;
-    containerRef: Container;
-
-    type?: string;
+    containerRef: Container;    // TODO: check usability
 }
