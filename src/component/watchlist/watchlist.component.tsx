0a30fdb251e62bb57b19631f093d620fab58e280
smarquezmgh <santiago.marquez@thedac.info>
2023-09-11 18:15:06 -0500

    loader component added

--- src/component/watchlist/watchlist.component.tsx
+++ src/component/watchlist/watchlist.component.tsx
@@ -2,22 +2,23 @@
 
 import { useAppSelector } from "../../state/hooks";
 import IsLoginUI from "../../ui/common/isLogin.ui";
+import LoaderUI from "../../ui/common/loader.ui";
 import WatchlistUI from "../../ui/watchlist/watchlist.ui";
 
 export default function WatchlistComponent() {
   const isConnected = useAppSelector(state => state.login.connected);
-  const watchlist = useAppSelector(state=> state.watchlist);
+  const watchlist = useAppSelector(state => state.watchlist);
   return (
     <>
       {!isConnected ?
         <IsLoginUI message="Please log in to show your watchlist" />
         :
         <>
-        {
-          watchlist.list !== undefined ? <WatchlistUI allLands={watchlist.list} /> 
-          :
-          <p>Loading...</p>
-        }
+          {
+            watchlist.list !== undefined ? <WatchlistUI allLands={watchlist.list} />
+              :
+              <LoaderUI size={100} text={"Loading lands..."}/>
+          }
         </>
       }
     </>
