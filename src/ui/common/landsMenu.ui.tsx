2477eafa16b839f5e6af6c0e47d3b0c62cc4fb30
smarquezmgh <santiago.marquez@thedac.info>
2023-09-29 09:55:00 -0500

    responsive menus added

--- src/ui/common/landsMenu.ui.tsx
+++ src/ui/common/landsMenu.ui.tsx
@@ -18,7 +18,7 @@ export default function LandsMenuUI({ metaverse, setMetaverse, form, isBorder }:
   }
 
   return (
-    <div className={`w-full flex items-center justify-center  py-16 ${isBorder ? "border-b border-nm-remark dark:border-nm-dm-fill" : ""}`}>
+    <div className={`w-full flex flex-wrap items-center justify-center  py-10 lg:py-16 ${isBorder ? "border-b border-nm-remark dark:border-nm-dm-fill" : ""}`}>
       {
         form == ButtonForm.Horizontal ?
           <>
@@ -26,7 +26,7 @@ export default function LandsMenuUI({ metaverse, setMetaverse, form, isBorder }:
               <button
                 key={iterateMetaverse}
                 type="button"
-                className={`flex items-center py-3 px-10 text-sm font-bold focus:outline-none rounded-xl mx-3 transition ease-in-out duration-300 bg-lm-fill dark:bg-nm-dm-fill  ${isAll(iterateMetaverse) ? "shadow-hollow-8 dark:shadow-dm-hollow-8  text-nm-dm-icons dark:text-nm-fill" : " shadow-relief-12 dark:shadow-dm-relief-12 hover:shadow-relief-32 dark:hover:shadow-dm-relief-32 text-nm-dm-remark dark:text-nm-fill"}`}
+                className={`w-[220px] flex items-center justify-center py-3 text-sm font-bold focus:outline-none rounded-xl mx-3 my-3 transition ease-in-out duration-300 bg-lm-fill dark:bg-nm-dm-fill  ${isAll(iterateMetaverse) ? "shadow-hollow-8 dark:shadow-dm-hollow-8  text-nm-dm-icons dark:text-nm-fill" : " shadow-relief-12 dark:shadow-dm-relief-12 hover:shadow-relief-32 dark:hover:shadow-dm-relief-32 text-nm-dm-remark dark:text-nm-fill"}`}
                 onClick={() => setMetaverse(iterateMetaverse === "All" ? undefined : iterateMetaverse)}
               >
                 {iterateMetaverse === Metaverses.SandBox &&
@@ -42,22 +42,41 @@ export default function LandsMenuUI({ metaverse, setMetaverse, form, isBorder }:
           :
           <>
             {Object.values(Metaverses).map((iterateMetaverse) =>
-              <button
-                key={iterateMetaverse}
-                type="button"
-                className={`flex flex-col items-center justify-center rounded-3xl cursor-pointer w-[240px] h-[320px] mx-10 focus:outline-none transition ease-in-out duration-300 grayscale hover:grayscale-0 shadow-relief-32 hover:shadow-relief-12 dark:shadow-dm-relief-32 dark:hover:shadow-dm-relief-12 ${metaverse === iterateMetaverse ? "grayscale-0" : ""}`}
-                onClick={() => setMetaverse(iterateMetaverse)}
-              >
-                {iterateMetaverse === Metaverses.SandBox &&
-                  <Image src="/images/the-sandbox-sand-logo.png" width={100} height={100} alt="sandbox" />}
-                {iterateMetaverse === Metaverses.Decentraland &&
-                  <Image src="/images/decentraland-mana-logo.png" width={100} height={100} alt="sandbox" />}
-                {iterateMetaverse === Metaverses.SomniumSpace &&
-                  <Image src="/images/somnium-space-cube-logo.webp" width={100} height={100} alt="sandbox" />}
-                <p className="mt-14 text-lg font-semibold text-lm-text dark:text-nm-fill">
-                  {METAVERSE_LABEL[iterateMetaverse].toUpperCase()}
-                </p>
-              </button>
+              <div key={iterateMetaverse}>
+                <button
+                  key={iterateMetaverse}
+                  type="button"
+                  className={`hidden lg:flex flex-col items-center justify-center rounded-3xl cursor-pointer w-[240px] h-[320px] mx-10 my-3 focus:outline-none transition ease-in-out duration-300 grayscale hover:grayscale-0 shadow-relief-32 hover:shadow-relief-12 dark:shadow-dm-relief-32 dark:hover:shadow-dm-relief-12 ${metaverse === iterateMetaverse ? "grayscale-0" : ""}`}
+                  onClick={() => setMetaverse(iterateMetaverse)}
+                >
+                  {iterateMetaverse === Metaverses.SandBox &&
+                    <Image src="/images/the-sandbox-sand-logo.png" width={100} height={100} alt="sandbox" />}
+                  {iterateMetaverse === Metaverses.Decentraland &&
+                    <Image src="/images/decentraland-mana-logo.png" width={100} height={100} alt="sandbox" />}
+                  {iterateMetaverse === Metaverses.SomniumSpace &&
+                    <Image src="/images/somnium-space-cube-logo.webp" width={100} height={100} alt="sandbox" />}
+                  <p className="mt-14 text-lg font-semibold text-lm-text dark:text-nm-fill">
+                    {METAVERSE_LABEL[iterateMetaverse].toUpperCase()}
+                  </p>
+                </button>
+                <button
+                  key={iterateMetaverse}
+                  type="button"
+                  className={`flex justify-center items-center py-3 w-[220px] text-sm font-bold focus:outline-none rounded-xl mx-3 my-4 transition ease-in-out duration-300 bg-lm-fill dark:bg-nm-dm-fill shadow-relief-12 dark:shadow-dm-relief-12 text-nm-dm-remark dark:text-nm-fill ${metaverse === iterateMetaverse ? "grayscale-0" : ""} block lg:hidden`}
+                  onClick={() => setMetaverse(iterateMetaverse)}
+                >
+                  {iterateMetaverse === Metaverses.SandBox &&
+                    <Image src="/images/the-sandbox-sand-logo.png" width={24} height={24} alt="sandbox"/>}
+                  {iterateMetaverse === Metaverses.Decentraland &&
+                    <Image src="/images/decentraland-mana-logo.png" width={24} height={24} alt="sandbox"/>}
+                  {iterateMetaverse === Metaverses.SomniumSpace &&
+                    <Image src="/images/somnium-space-cube-logo.webp" width={24} height={24} alt="sandbox"/>}
+                    <p className="pl-2">
+                      {METAVERSE_LABEL[iterateMetaverse].toUpperCase()}
+                    </p>
+                </button>
+
+              </div>
             )}
           </>
       }
--- src/ui/header/header.ui.tsx
+++ src/ui/header/header.ui.tsx
@@ -1,18 +1,98 @@
+import { useState } from "react";
+import Link from "next/link";
+import Image from 'next/image';
 import ConnectButtonUI from "../common/connectButton.ui";
 import DarkModeButtonUI from "../common/darkModeButton.ui";
+import NavButton from "../navbar/navbarButton.ui";
+
+interface ListProps {
+  url: string;
+  label: string;
+  icon: string;
+  isExternal: boolean;
+}
+
+const list = [
+  {
+    url: "/metaverseexplorer",
+    label: "Metaverse explorer",
+    icon: "b",
+    isExternal: false,
+  },
+  {
+    url: "/stake",
+    label: "Stake MGH",
+    icon: "d",
+    isExternal: false,
+  },
+  {
+    url: "https://snapshot.org/#/metagamehub.eth",
+    label: "Governance",
+    icon: "a",
+    isExternal: true,
+  },
+];
 
 interface HeaderUIProp {
   isConnected: boolean;
 }
 
 export default function HeaderUI({ isConnected }: HeaderUIProp) {
+  const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
+  const handleToggleClick = () => {
+    setIsNavOpen((prev) => !prev);
+  };
 
   return (
-    <header className={`${isConnected ? "bg-[url('/images/banner.png')] dark:bg-[url('/images/dm-banner.png')]" : ""}`}  >
-      <div className='mr-12 relative float-right flex'>
-        <ConnectButtonUI />
-        <DarkModeButtonUI/>
-      </div>
-    </header>
+    <>
+      <header className={`${isConnected ? "bg-[url('/images/banner.png')] dark:bg-[url('/images/dm-banner.png')]" : ""} hidden lg:block`}  >
+        <div className='mr-16 relative float-right flex'>
+          <ConnectButtonUI />
+          <DarkModeButtonUI />
+        </div>
+      </header>
+      <nav className="mt-7 block lg:hidden">
+        <div className="flex justify-between items-center">
+          <Link href={"/metaverseexplorer"}>
+            <Image
+              src={'/images/mgh_logo/mgh_logo.svg'}
+              alt={'TheHub DAO logo'}
+              width={45}
+              height={43}
+              className="cursor-pointer flex justify-start items-start ml-4"
+            />
+          </Link>
+          <div className={`font-toogle text-3xl pr-4 cursor-pointer ${isNavOpen ? 'hidden' : ''}`} onClick={() => { handleToggleClick() }}>
+            b
+          </div>
+        </div>
+        {isNavOpen && (
+          <>
+            <div className="fixed w-full h-screen inset-0 z-20">
+              <div className='absolute top-0 right-0 h-screen w-full bg-lm-fill dark:bg-nm-black flex flex-col justify-center px-8 items-center py-20'>
+              <div className="flex flex-col space-y-4 items-center">
+                  {
+                    list.map((option: ListProps) => {
+                      return(
+                        <div key={option.label}>
+                          <NavButton url={option.url} label={option.label} icon={option.icon} isExternal={option.isExternal}/>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+                <div className="flex flex-wrap">
+                  <ConnectButtonUI />
+                </div>
+                <DarkModeButtonUI />
+                <div className="absolute top-0 right-0 pr-4 pt-9 font-toogle text-xl cursor-pointer" onClick={handleToggleClick}>
+                  x
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </nav>
+    </>
   )
 }
