9144f1f5a34446491e7819909cc90e3c2f793335
smarquezmgh <santiago.marquez@thedac.info>
2023-10-02 13:22:07 -0500

    fixed dark mode for loader icon

--- src/ui/common/logo.ui.tsx
+++ src/ui/common/logo.ui.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
-
+import { useTheme } from "next-themes";
 
 export default function LogoUI (){
     const maksElement = useRef(null);
     const lightLogoElement = useRef(null);
     const darkLogoElement = useRef(null);
+    const { resolvedTheme } = useTheme();
   
     useEffect(() => {
       const logoTl = gsap.timeline({ paused: true, repeat: -1, repeatDelay: .4 });
@@ -77,10 +78,10 @@ export default function LogoUI (){
           </clipPath>
         </defs>
         {/* external grey logo */}
-        <path d="M95.728,52.103C107.852,72.203 92.731,97.281 68.483,97.281C68.483,97.281 54.699,92.21 49.997,92.21C45.295,92.21 31.511,97.281 31.511,97.281C7.309,97.281 -7.858,72.203 4.266,52.103C4.266,52.103 15.607,43.667 17.958,39.748C20.263,35.922 22.752,21.447 22.752,21.447C34.876,1.347 65.118,1.347 77.242,21.447C77.242,21.447 79.501,35.507 81.806,39.333C84.157,43.252 95.728,52.103 95.728,52.103Z" style={{ fill: 'rgb(24, 175, 242)' }} transform='translate(75, 65)' ref={darkLogoElement} />
+        <path d="M95.728,52.103C107.852,72.203 92.731,97.281 68.483,97.281C68.483,97.281 54.699,92.21 49.997,92.21C45.295,92.21 31.511,97.281 31.511,97.281C7.309,97.281 -7.858,72.203 4.266,52.103C4.266,52.103 15.607,43.667 17.958,39.748C20.263,35.922 22.752,21.447 22.752,21.447C34.876,1.347 65.118,1.347 77.242,21.447C77.242,21.447 79.501,35.507 81.806,39.333C84.157,43.252 95.728,52.103 95.728,52.103Z" style={{ fill: `${resolvedTheme === 'dark' ? "rgb(49, 47, 48)":"rgb(24, 175, 242"}` }} transform='translate(75, 65)' ref={darkLogoElement} />
         {/* white logo with grey circle group */}
         <g clipPath='url(#svgMask)'>
-          <circle cx={125} cy={125} r={125} fill={'rgb(24, 175, 242)'} />
+          <circle cx={125} cy={125} r={125} fill={`${resolvedTheme === 'dark' ? "rgb(49, 47, 48)":"rgb(24, 175, 242"}`} />
           <path d="M95.728,52.103C107.852,72.203 92.731,97.281 68.483,97.281C68.483,97.281 54.699,92.21 49.997,92.21C45.295,92.21 31.511,97.281 31.511,97.281C7.309,97.281 -7.858,72.203 4.266,52.103C4.266,52.103 15.607,43.667 17.958,39.748C20.263,35.922 22.752,21.447 22.752,21.447C34.876,1.347 65.118,1.347 77.242,21.447C77.242,21.447 79.501,35.507 81.806,39.333C84.157,43.252 95.728,52.103 95.728,52.103Z" style={{ fill: 'white' }} transform='translate(75, 65)' ref={lightLogoElement} />
         </g>
       </svg>
