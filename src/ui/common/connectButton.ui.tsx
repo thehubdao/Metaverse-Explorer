361e5bb4975917b0576c18bf3d06fa9c6b5d6fd1
smarquezmgh <santiago.marquez@thedac.info>
2023-10-04 12:49:52 -0500

    buttons state fixed

--- src/ui/common/connectButton.ui.tsx
+++ src/ui/common/connectButton.ui.tsx
@@ -1,10 +1,17 @@
+import { useEffect, useState } from "react";
 import { Login } from "../../component/login.component"
 import { useTheme } from "next-themes";
 
 export default function ConnectButtonUI() {
   const { resolvedTheme } = useTheme();
+  const [buttonState, setButtonState] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (resolvedTheme === "dark") setButtonState(true);
+    else setButtonState(false);
+  }, [resolvedTheme]);
   return (
-    <div className={`mt-6 cursor-pointer lg:px-4 flex justify-center items-center  ${resolvedTheme === 'dark' ? "dark-connect" : ""}`}>
+    <div className={`mt-6 cursor-pointer lg:px-4 flex justify-center items-center  ${buttonState ? "dark-connect" : ""}`}>
       <Login />
     </div>
   )
--- src/ui/common/darkModeButton.ui.tsx
+++ src/ui/common/darkModeButton.ui.tsx
@@ -1,15 +1,23 @@
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
-export default function DarkModeButtonUI(){
-    const { theme, setTheme } = useTheme();
-    const { resolvedTheme } = useTheme();
-    return(
-        <div className="w-[77px] h-[39px] bg-[#D3D9E6] shadow-relief-12 dark:bg-[#3B434E] rounded-3xl border-4 border-nm-fill dark:border-nm-dm-fill dark:shadow-dark-toogle mt-6 cursor-pointer" onClick={() => theme == "dark" ? setTheme("light") : setTheme("dark")}>
-            <div className={`w-[31px] h-[31px] flex justify-center items-center bg-nm-gray dark:bg-nm-dm-fill shadow-toogle dark:shadow-dark-toogle rounded-full transition-all duration-300 ${resolvedTheme === 'dark' ? "translate-x-[37px]" : "translate-x-0"}`}>
-                <div className="w-[21px] h-[21px] bg-nm-gray dark:bg-nm-dm-fill shadow-toogle dark:shadow-dark-toogle rounded-full">
+export default function DarkModeButtonUI() {
+  const { theme, setTheme } = useTheme();
+  const { resolvedTheme } = useTheme();
+  const [buttonState, setButtonState] = useState<boolean>(false);
 
-                </div>
-            </div>
+  useEffect(() => {
+    if (resolvedTheme === "dark") setButtonState(true);
+    else setButtonState(false);
+  }, [resolvedTheme]);
+
+  return (
+    <div className="w-[77px] h-[39px] bg-[#D3D9E6] shadow-relief-12 dark:bg-[#3B434E] rounded-3xl border-4 border-nm-fill dark:border-nm-dm-fill dark:shadow-dark-toogle mt-6 cursor-pointer" onClick={() => theme == "dark" ? setTheme("light") : setTheme("dark")}>
+      <div className={`w-[31px] h-[31px] flex justify-center items-center bg-nm-gray dark:bg-nm-dm-fill shadow-toogle dark:shadow-dark-toogle rounded-full transition-all duration-300 ${buttonState ? "translate-x-[37px]" : "translate-x-0"}`}>
+        <div className="w-[21px] h-[21px] bg-nm-gray dark:bg-nm-dm-fill shadow-toogle dark:shadow-dark-toogle rounded-full">
+
         </div>
-    )
+      </div>
+    </div>
+  )
 }
