0898f6a081ab421ef038fd60895face2b5e5ea20
smarquezmgh <santiago.marquez@thedac.info>
2023-10-05 19:40:35 -0500

    responsive for tables added

--- src/ui/heatmap/topSellsLands.ui.tsx
+++ src/ui/heatmap/topSellsLands.ui.tsx
@@ -41,55 +41,62 @@ export default function TopSellsLandsUI({ tableData, title, headers }: TopSellsL
 
   return (
     <>
-      <div className="flex justify-between">
-        <p className="text-lm-text dark:text-nm-fill text-lg font-semibold mb-9">{title}</p>
+      <div className="flex flex-wrap lg:hidden justify-between mt-10 mb-4">
+        <p className="text-lm-text dark:text-nm-fill text-lg font-semibold mb-4">{title}</p>
         <TopSellsFilterUI filterBy={filterBy} setFilterBy={setFilterBy} />
       </div>
-      <table className="w-full table-fixed border-collapse">
-        <thead className="w-full">
-          <tr className="flex w-full items-center text-lm-text dark:text-nm-fill font-bold bg-nm-gray dark:bg-[#32363C] rounded-xl">
-            {headers.map((header, index) => (
-              <th key={index} className="p-4 w-1/5 text-lg rounded-2xl">
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody className="flex-col items-center justify-between w-full">
-          {currentPageData ? currentPageData.map((item, index) => (
-            <tr key={index} className="flex w-full items-center">
-              <>
-                <td className="p-4 w-1/5 text-lg rounded-2xl flex justify-center">{item.position}</td>
-                <Tooltip title={item.dataTable?.asset} placement="top">
-                  <td className="p-4 w-1/5 text-lg rounded-2xl flex  items-center">
-                    <Link key={index} href={item.dataTable?.external_link}>
-                      <div className="relative h-12 w-12 rounded-full">
-                        <Image
-                          src={item.dataTable?.image ?? ""}
-                          fill
-                          className="rounded-full"
-                          alt="land image"
-                        />
+
+      <div className="bg-lm-fill dark:bg-nm-dm-fill rounded-3xl w-full lg:mt-10 lg:py-10 lg:px-12 relative overflow-x-auto">
+        <div className="justify-between hidden lg:flex">
+          <p className="text-lm-text dark:text-nm-fill text-lg font-semibold mb-9">{title}</p>
+          <TopSellsFilterUI filterBy={filterBy} setFilterBy={setFilterBy} />
+        </div>
+        <table className="w-full min-w-[664px]">
+          <thead className="w-full">
+            <tr className="flex w-full items-center text-lm-text dark:text-nm-fill font-bold bg-nm-gray dark:bg-[#32363C] rounded-t lg:rounded-xl">
+              {headers.map((header, index) => (
+                <th key={index} className="p-4 w-1/5 text-lg">
+                  {header}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="flex-col items-center justify-between w-full">
+            {currentPageData ? currentPageData.map((item, index) => (
+              <tr key={index} className="flex w-full items-center">
+                <>
+                  <td className="p-4 w-1/5 text-lg flex justify-center">{item.position}</td>
+                  <Tooltip title={item.dataTable?.asset} placement="top">
+                    <td className="p-4 w-1/5 text-lg flex  items-center">
+                      <Link key={index} href={item.dataTable?.external_link}>
+                        <div className="relative h-12 w-12 rounded-full">
+                          <Image
+                            src={item.dataTable?.image ?? ""}
+                            fill
+                            className="rounded-full"
+                            alt="land image"
+                          />
+                        </div>
+                      </Link>
+                      <div className="truncate pl-2">
+                        {item.dataTable?.asset}
                       </div>
-                    </Link>
-                    <div className="truncate pl-2">
-                      {item.dataTable?.asset}
-                    </div>
-                  </td>
-                </Tooltip>
-                <td className="p-4 w-1/5 text-lg rounded-2xl flex justify-center">not available</td>
-                <td className="p-4 w-1/5 text-lg rounded-2xl flex justify-center">anonymous</td>
-                <td className="p-4 w-1/5 text-lg rounded-2xl flex justify-center">{item.dataTable?.date?.toString() ?? "no date available"}</td>
-              </>
-            </tr>
-          ))
-            :
-            <tr className="w-full h-full flex justify-center items-center">
-              <th className="my-20">At this moment we have no top from this metaverse.</th>
-            </tr>
-          }
-        </tbody>
-      </table>
+                    </td>
+                  </Tooltip>
+                  <td className="p-4 w-1/5 text-lg flex justify-center">not available</td>
+                  <td className="p-4 w-1/5 text-lg flex justify-center">anonymous</td>
+                  <td className="p-4 w-1/5 text-lg flex justify-center">{item.dataTable?.date?.toString() ?? "no date available"}</td>
+                </>
+              </tr>
+            ))
+              :
+              <tr className="w-full h-full flex justify-center items-center">
+                <th className="my-20">At this moment we have no top from this metaverse.</th>
+              </tr>
+            }
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
