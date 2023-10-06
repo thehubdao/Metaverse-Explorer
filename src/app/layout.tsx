778f5c72d68b61bb0a32f27d2d15eb5f3cc1a72d
smarquezmgh <santiago.marquez@thedac.info>
2023-10-05 19:37:40 -0500

    url images added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,14 +51,20 @@ const subHeaderList = [
   {
     name: "Heatmap",
     route: "metaverseexplorer",
+    image: "/images/icons/menu/LANDVALUATION_ON.svg",
+    darkImage: "/images/icons/menu/LANDVALUATION_WHITE.svg"
   },
   {
     name: "Portfolio",
     route: "metaverseexplorer/portfolio",
+    image: "/images/icons/menu/PORTFOLIO_ON.svg",
+    darkImage: "/images/icons/menu/PORTFOLIO_WHITE.svg"
   },
   {
     name: "Watchlist",
     route: "metaverseexplorer/watchlist",
+    image: "/images/icons/menu/WATCHLIST_ON.svg",
+    darkImage: "/images/icons/menu/WATCHLIST_WHITE.svg"
   },
   // {
   //   name: "Analytics",
@@ -84,7 +90,7 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
     <Provider store={store}>
       <html lang="en" className={`${plusJakarta.variable} ${fontIcons.variable} ${toogleIcons.variable}`}>
         <body className="font-plus text-nm-dm-highlight dark:text-nm-fill">
-          <ThemeProvider attribute="class">
+          <ThemeProvider attribute="class" disableTransitionOnChange>
             <WagmiProvider>
               <div className={`w-full h-screen block lg:grid lg:grid-cols-[137px_calc(100%-137px)] ${isConnected ? 'lg:grid-rows-[226px_1fr]' : 'lg:grid-rows-[70px_1fr]'} `}>
                 <nav className="row-span-2 hidden lg:block">
