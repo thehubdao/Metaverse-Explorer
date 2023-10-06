32f3aa8c933170e3651849f23b645857b352b5f1
smarquezmgh <santiago.marquez@thedac.info>
2023-10-05 19:38:42 -0500

    unneccesary code removed

--- src/ui/common/connectButton.ui.tsx
+++ src/ui/common/connectButton.ui.tsx
@@ -1,17 +1,9 @@
-import { useEffect, useState } from "react";
 import { Login } from "../../component/login.component"
-import { useTheme } from "next-themes";
 
 export default function ConnectButtonUI() {
-  const { resolvedTheme } = useTheme();
-  const [buttonState, setButtonState] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (resolvedTheme === "dark") setButtonState(true);
-    else setButtonState(false);
-  }, [resolvedTheme]);
   return (
-    <div className={`mt-6 cursor-pointer lg:px-4 flex justify-center items-center  ${buttonState ? "dark-connect" : ""}`}>
+    <div className="cursor-pointer lg:px-4 flex justify-center items-center dark:dark-connect">
       <Login />
     </div>
   )
