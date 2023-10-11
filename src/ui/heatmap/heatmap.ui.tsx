d611b3f16d48df8067cd5994122d230d971e337a
smarquezmgh <santiago.marquez@thedac.info>
2023-10-10 20:17:00 -0500

    add text to responsive view

--- src/ui/heatmap/heatmap.ui.tsx
+++ src/ui/heatmap/heatmap.ui.tsx
@@ -83,7 +83,7 @@ export default function HeatmapUI({ globalData, topPicksLands, topSellingsLands 
             {metaverseSelected &&
               <>
                 <EstimatorValuesUI metaverseSelected={metaverseSelected} info={`THE HUB LAND price estimator uses AI to calculate the fair value of LANDs and help you find undervalued ones.  Leverage our heatmap to quickly get an overview of ${metaverseSelected} Map and get insights about current price trends. The valuations are updated at a daily basis.`} globalData={globalData} />
-                <div className="rounded-3xl p-7 shadow-relief-12 dark:shadow-dm-relief-12 h-[80vh]">
+                <div className="rounded-3xl p-7 shadow-relief-12 dark:shadow-dm-relief-12 h-[80vh] hidden lg:flex">
                   <div ref={heatmapDivRef} className="w-full h-full relative">
                     <div className="absolute top-1 left-1 z-20 flex gap-4 md:w-fit w-full m-4">
                       {/* Metaverse Selection */}
@@ -97,6 +97,7 @@ export default function HeatmapUI({ globalData, topPicksLands, topSellingsLands 
                       metaverse={metaverseSelected} renderAfter={false} onClickLand={onClickLand} initialX={0} initialY={0} />
                   </div>
                 </div>
+                <h1 className="text-xl font-bold dark:text-nm-highlight block lg:hidden text-center">To use heatmap go to desktop version</h1>
                 <div>
                   <div className="flex items-center justify-center mt-7 text-lm-text dark:text-nm-highlight">
                     <div className="flex flex-col justify-center text-center">
