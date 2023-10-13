5b8dc435fb1cf2bdbef7379992763d1366ea3772
smarquezmgh <santiago.marquez@thedac.info>
2023-10-12 19:39:32 -0500

    dummy data components calls added

--- src/ui/heatmap/heatmap.ui.tsx
+++ src/ui/heatmap/heatmap.ui.tsx
@@ -20,6 +20,10 @@ import MapChooseMetaverseUI from "./mapChooseMetaverse.ui";
 import { MapFilter } from "../../types/heatmap/heatmap.type";
 import MapChooseFilterUI from "./mapChooseFilter.ui";
 import MapSearchUI from "./mapSearch.ui";
+import FullScreenButtonUI from "../common/fullScreenButton.ui";
+import { LegendFilter } from "../../enums/heatmap/filter.enum";
+import MapLegendUI from "./mapLegend.ui";
+import MapCardUI from "./mapCard.ui";
 
 
 const headersPicks = [
@@ -50,6 +54,9 @@ export default function HeatmapUI({ globalData, topPicksLands, topSellingsLands 
   const [selectCoord, setSelectCoord] = useState<boolean>(false);
   const [landId, setLandId] = useState<number | undefined>(undefined);
   const [coordinates, setCoordinates] = useState<{ X: number | undefined; Y: number | undefined }>({ X: undefined, Y: undefined });
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+  const [openSpecificModal, setOpenSpecificModal] = useState<boolean>(false);
+  const [legendFilter, setLegendFilter] = useState<LegendFilter | undefined>();
 
   const filterLands = (metaverse: Metaverses | undefined) => {
     dispatch(setHeatmapMetaverse(metaverse));
@@ -57,6 +64,71 @@ export default function HeatmapUI({ globalData, topPicksLands, topSellingsLands 
 
   function onClickLand(land: LandTileData) {
     console.warn(land);
+  }
+  
+  const data = {
+    "apiData": {
+      "tokenId": "3062541302288446171170371466885913903138",
+      "name": "Parcel 9,34",
+      "images": {
+        "image_url": "https://lh3.googleusercontent.com/qrlWtc6u8a4ahxhkvIB7tVQcs8ISPoSuoTu-lyXYXNt-Az7VZC7Tve9J1MezletKSma0-NSsd2LqnrsDiL3Cf48bbk5IRl4O4c5z6g",
+        "image_preview_url": "https://lh3.googleusercontent.com/qrlWtc6u8a4ahxhkvIB7tVQcs8ISPoSuoTu-lyXYXNt-Az7VZC7Tve9J1MezletKSma0-NSsd2LqnrsDiL3Cf48bbk5IRl4O4c5z6g=s250",
+        "image_thumbnail_url": "https://lh3.googleusercontent.com/qrlWtc6u8a4ahxhkvIB7tVQcs8ISPoSuoTu-lyXYXNt-Az7VZC7Tve9J1MezletKSma0-NSsd2LqnrsDiL3Cf48bbk5IRl4O4c5z6g=s128",
+        "image_original_url": "https://api.decentraland.org/v1/parcels/9/34/map.png?size=24&width=1024&height=1024"
+      },
+      "external_link": "https://market.decentraland.org/contracts/0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d/tokens/3062541302288446171170371466885913903138",
+      "market_links": {
+        "opensea": "https://opensea.io/assets/ethereum/0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d/3062541302288446171170371466885913903138",
+        "looksrare": "https://looksrare.org/collections/0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d/3062541302288446171170371466885913903138",
+        "X2Y2": "https://x2y2.io/eth/0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d/3062541302288446171170371466885913903138"
+      },
+      "token_metadata": "https://api.decentraland.org/v2/contracts/0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d/tokens/3062541302288446171170371466885913903138",
+      "predicted_price": 12079.86556076581,
+      "eth_predicted_price": 2.170321630188178,
+      "floor_adjusted_predicted_price": 2.170321630188178,
+      "coords": {
+        "x": 9,
+        "y": -34
+      },
+      "history": [
+        {
+          "timestamp": 1674816767000,
+          "time": "2023-01-27 10:52:47",
+          "hash": "0x77711d03fdee4b247e47b7cc7d7afe42e42be3e0a612709ea68f92fc125a0d0f",
+          "action": "Add Land to Estate",
+          "owner": "0x959e104e1a4db6317fa58f8295f586e1a978c297",
+          "chain": "ethereum",
+          "valuation": 6.54370564613492,
+          "estateId": "5415"
+        }
+      ],
+      "variation_last_week": null,
+      "variation_last_four_weeks": null,
+      "variation_last_six_months": null,
+      "tile": {
+        "type": 10,
+        "top": 1
+      },
+      "owner": "0x1e2fef88db7cb74691a21a39c9cb44bb4df52af5",
+      "estateId": "5415",
+      "current_price": 5833.333333333333,
+      "current_price_eth": 1.035661347575525,
+      "metaverse": "decentraland",
+      "prices": {
+        "eth_predicted_price": 2.170321630188178,
+        "predicted_price": 0
+      }
+    },
+    "predictions": {
+      "ethPrediction": 2.170321630188178,
+      "usdPrediction": 0,
+      "metaversePrediction": null
+    },
+    "landCoords": {
+      "x": 9,
+      "y": 34
+    },
+    "name": "Parcel 9,34"
   }
 
   return (
@@ -84,6 +156,7 @@ export default function HeatmapUI({ globalData, topPicksLands, topSellingsLands 
               <>
                 <EstimatorValuesUI metaverseSelected={metaverseSelected} info={`THE HUB LAND price estimator uses AI to calculate the fair value of LANDs and help you find undervalued ones.  Leverage our heatmap to quickly get an overview of ${metaverseSelected} Map and get insights about current price trends. The valuations are updated at a daily basis.`} globalData={globalData} />
                 <div className="rounded-3xl p-7 shadow-relief-12 dark:shadow-dm-relief-12 h-[80vh] hidden lg:flex">
+                  <button className="w-6 h-6 bg-red-600 rounded-2xl top-1/2 left-1/2 relative cursor-pointer z-20" onClick={() => setIsVisible(false)}></button>
                   <div ref={heatmapDivRef} className="w-full h-full relative">
                     <div className="absolute top-1 left-1 z-20 flex gap-4 md:w-fit w-full m-4">
                       {/* Metaverse Selection */}
@@ -93,8 +166,25 @@ export default function HeatmapUI({ globalData, topPicksLands, topSellingsLands 
                       {/* Search by coords */}
                       <MapSearchUI selectCoord={selectCoord} setSelectCoord={setSelectCoord} setSelectfilter={setSelectFilter} setSelectMetaverse={setSelectMetaverse} setCoordinates={setCoordinates} coordinates={coordinates} landId={landId} setLandId={setLandId} />
                     </div>
-                    <Heatmap2D viewportWidth={heatmapDivRef.current?.offsetWidth ?? window.innerWidth} viewportHeight={heatmapDivRef.current?.offsetHeight ?? window.innerHeight}
-                      metaverse={metaverseSelected} renderAfter={false} onClickLand={onClickLand} initialX={0} initialY={0} />
+                    {
+                      isVisible &&
+                      <div className="absolute z-20 top-1 right-1 rounded-full bg-nm-fill m-4 p-2 h-9 w-9">
+                        <FullScreenButtonUI
+                          fullScreenRef={heatmapDivRef}
+                        />
+                      </div>
+                    }
+                    {/* <Heatmap2D viewportWidth={heatmapDivRef.current?.offsetWidth ?? window.innerWidth} viewportHeight={heatmapDivRef.current?.offsetHeight ?? window.innerHeight}
+                      metaverse={metaverseSelected} renderAfter={false} onClickLand={onClickLand} initialX={0} initialY={0} /> */}
+                    {
+                      filterBy === "basic" && isVisible &&
+                      <MapLegendUI legendFilter={legendFilter} setLegendFilter={setLegendFilter} metaverse={metaverseSelected} />
+                    }
+                    {!isVisible &&
+                      <div className="absolute bottom-16 right-1 flex flex-col gap-4 m-4">
+                        <MapCardUI landData={data} metaverse={metaverseSelected} setIsVisible={setIsVisible} setOpenSpecificModal={setOpenSpecificModal} />
+                      </div>
+                    }
                   </div>
                 </div>
                 <h1 className="text-xl font-bold dark:text-nm-highlight block lg:hidden text-center">To use heatmap go to desktop version</h1>
@@ -115,10 +205,10 @@ export default function HeatmapUI({ globalData, topPicksLands, topSellingsLands 
                     <BoxInformationUI title={"Floor Price:"} prices={coinPrices} />
                     <BoxInformationUI title={"Estimate Accuracy:"} prices={coinPrices} />
                   </div>
-                  <div className="w-[580px] h-[205px] bg-lm-fill dark:bg-nm-dm-fill rounded-3xl flex flex-col items-center justify-center my-2 2xl:ml-4">
+                  {/* <div className="w-[580px] h-[205px] bg-lm-fill dark:bg-nm-dm-fill rounded-3xl flex flex-col items-center justify-center my-2 2xl:ml-4">
                     <h1>Graph</h1>
                     <p>coming soon</p>
-                  </div>
+                  </div> */}
                 </div>
                 <TopLandsUI tableData={topPicksLands} title="Our Top Picks" headers={headersPicks} />
                 <TopSellsLandsUI tableData={topSellingsLands} title="Our Top Sells" headers={headersSells} />
