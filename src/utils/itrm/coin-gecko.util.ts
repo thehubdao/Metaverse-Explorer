75c703c6d33b96722c11316b92fd91cb8c858769
smarquezmgh <santiago.marquez@thedac.info>
2023-10-18 11:14:40 -0500

    temporal config

--- src/utils/itrm/coin-gecko.util.ts
+++ src/utils/itrm/coin-gecko.util.ts
@@ -1,5 +1,5 @@
 ﻿import axios from "axios";
-import {Raise} from "../common.util";
+// import {Raise} from "../common.util";
 import {LogError} from "../logging.util";
 import {Module} from "../../enums/logging.enum";
 
@@ -41,9 +41,10 @@ interface CoinGeckoResponse {
 
 export async function GetCoinGeckoPrices(): Promise<CoinValuesType> {
   try {
-    const itrmServiceUrl = process.env.NEXT_PUBLIC_ITRM_SERVICE ?? Raise("Missing ITRMService url on env variables!");
+    // const itrmServiceUrl = process.env.NEXT_PUBLIC_ITRM_SERVICE ?? Raise("Missing ITRMService url on env variables!");
 
-    const response = await axios.get<CoinGeckoResponse>(`${itrmServiceUrl}/val-analytics/coingeckoResponse`);
+    // const response = await axios.get<CoinGeckoResponse>(`${itrmServiceUrl}/val-analytics/coingeckoResponse`);
+    const response = await axios.get<CoinGeckoResponse>(`https://api.coingecko.com/api/v3/simple/price?ids=ethereum%2Cthe-sandbox%2Caxie-infinity%2Cdecentraland%2Cocean-protocol%2Cmetagamehub-dao%2Cwmatic%2Cusd-coin%2Ctether%2Csomnium-space-cubes&vs_currencies=usd`);
     return response.data.success;
   } catch (err) {
     LogError(Module.CoinGecko, "Error trying to get CoinGecko prices from ITRM!", err);
