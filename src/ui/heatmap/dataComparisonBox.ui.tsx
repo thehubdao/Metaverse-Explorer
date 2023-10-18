94004ce95c7d7d7f06c09b21d1813a77a53f7a76
smarquezmgh <santiago.marquez@thedac.info>
2023-10-18 11:11:24 -0500

    nan value removed

--- src/ui/heatmap/dataComparisonBox.ui.tsx
+++ src/ui/heatmap/dataComparisonBox.ui.tsx
@@ -3,38 +3,37 @@ import Image from 'next/image';
 
 interface DataComparisonBoxUIProps {
   currentPriceEth: number | undefined;
-  predictions: IPredictions
+  predictions: IPredictions;
 }
 export default function DataComparisonBoxUI({ currentPriceEth, predictions }: DataComparisonBoxUIProps) {
-  const ethPredictionPrice = predictions?.ethPrediction
+  const ethPredictionPrice = predictions?.ethPrediction;
   const comparedValue =
     !ethPredictionPrice || !currentPriceEth
-      ? NaN
-      : ((currentPriceEth - ethPredictionPrice) / ethPredictionPrice) * 100
-  const isUnderValued = comparedValue < 0
+      ? "No Data"
+      : ((currentPriceEth - ethPredictionPrice) / ethPredictionPrice) * 100;
+  const isUnderValued = comparedValue !== "No Data" && comparedValue < 0;
   return (
     <div className='relative flex text-base text-left font-bold items-center gap-5'>
       {currentPriceEth
-        ? <div className='flex'>
-          <Image src={'/images/eth.svg'} width={20} height={20} className='rounded-full' alt="logo"/>
-          <p className={`text-lg`}>
-            {`${currentPriceEth?.toFixed(2)} ETH`}
-          </p>
-        </div>
-        : <p className={`text-lg`}>Not Listed</p>
-      }
-      {/* Comparison Percentage  */}
-      {
-        currentPriceEth && (
+        ?
+        <>
+          <div className='flex'>
+            <Image src={'/images/eth.svg'} width={20} height={20} className='rounded-full' alt="logo" />
+            <p className={`text-lg`}>
+              {`${currentPriceEth?.toFixed(2)} ETH`}
+            </p>
+          </div>
+          {/* Comparison Percentage  */}
           <div className={`${isUnderValued ? 'bg-[#47E298]' : 'bg-[#FF4949]'} text-white flex flex-col items-center px-2 py-1 rounded-lg`}>
             <p className='text-base leading-3'>
-              {`${Math.abs(comparedValue).toFixed()}%`}
+              {comparedValue !== "No Data" && `${Math.abs(comparedValue).toFixed()}%`}
             </p>
             <p className='text-xs leading-3'>
               {isUnderValued ? 'Undervalued' : 'Overvalued'}
             </p>
           </div>
-        )
+        </>
+        : <p className={`text-lg`}>Not Listed</p>
       }
     </div >
   )
