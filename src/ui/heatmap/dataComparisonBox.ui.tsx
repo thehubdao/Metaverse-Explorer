46effb3a779bb4e2d1daa57c041de677843ff3f6
FrancyAPinzonRNova <santiago.marquez@thedac.info>
2023-10-25 22:09:31 -0500

    alt attribute and comparedValue fixed

--- src/ui/heatmap/dataComparisonBox.ui.tsx
+++ src/ui/heatmap/dataComparisonBox.ui.tsx
@@ -9,16 +9,16 @@ export default function DataComparisonBoxUI({ currentPriceEth, predictions }: Da
   const ethPredictionPrice = predictions?.ethPrediction;
   const comparedValue =
     !ethPredictionPrice || !currentPriceEth
-      ? "No Data"
+      ? undefined
       : ((currentPriceEth - ethPredictionPrice) / ethPredictionPrice) * 100;
-  const isUnderValued = comparedValue !== "No Data" && comparedValue < 0;
+  const isUnderValued = comparedValue && comparedValue < 0;
   return (
     <div className='relative flex text-base text-left font-bold items-center gap-5'>
       {currentPriceEth
         ?
         <>
           <div className='flex'>
-            <Image src="/images/eth.svg" width={20} height={20} className='rounded-full' alt="logo" />
+            <Image src="/images/eth.svg" width={20} height={20} className='rounded-full' alt="Eth icon" />
             <p className={`text-lg`}>
               {`${currentPriceEth?.toFixed(2)} ETH`}
             </p>
@@ -26,7 +26,7 @@ export default function DataComparisonBoxUI({ currentPriceEth, predictions }: Da
           {/* Comparison Percentage  */}
           <div className={`${isUnderValued ? 'bg-[#47E298]' : 'bg-[#FF4949]'} text-white flex flex-col items-center px-2 py-1 rounded-lg`}>
             <p className='text-base leading-3'>
-              {comparedValue !== "No Data" && `${Math.abs(comparedValue).toFixed()}%`}
+              {comparedValue ? `${Math.abs(comparedValue).toFixed()}%` : "No data"}
             </p>
             <p className='text-xs leading-3'>
               {isUnderValued ? 'Undervalued' : 'Overvalued'}
