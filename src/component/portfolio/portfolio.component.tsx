acfaf0245f39324c57f2e6232f009558ef203370
FrancyAPinzonRNova <santiago.marquez@thedac.info>
2023-11-02 18:29:14 -0500

    fixed loader for waiting signature and added message error

--- src/component/portfolio/portfolio.component.tsx
+++ src/component/portfolio/portfolio.component.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { BiMessageSquareError } from "react-icons/bi";
 import { useAppSelector } from "../../state/hooks";
 import IsLoginUI from "../../ui/common/isLogin.ui";
 import LoaderUI from "../../ui/common/loader.ui";
@@ -8,6 +9,8 @@ import PortfolioUI from "../../ui/portfolio/portfolio.ui";
 export default function PortfolioComponent() {
   const isConnected = useAppSelector(state => state.login.connected);
   const portfolio = useAppSelector(state => state.portfolio);
+  const token = useAppSelector(state => state.login.accessToken?.token);
+  const address = useAppSelector(state => state.login.address);
 
   return (
     <>
@@ -16,9 +19,27 @@ export default function PortfolioComponent() {
         :
         <>
           {
-            portfolio.list !== undefined ? <PortfolioUI allLands={portfolio.list} landsOwned={portfolio.length ?? 0} totalWorth={portfolio.totalWorth}/>
+            address && token ?
+              <>
+                {
+                  !portfolio.isLoading ? 
+                  <>
+                  {
+                    portfolio.error ?
+                    <div className="pt-20 flex justify-center items-center gap-x-3">
+                      <BiMessageSquareError  className="text-3xl text-danger"/>
+                      <p>Upss, something is wrong, try later.</p>
+                    </div>
+                    :
+                    <PortfolioUI allLands={portfolio.list} landsOwned={portfolio.length ?? 0} totalWorth={portfolio.totalWorth} />
+                  }
+                  </>
+                    :
+                    <LoaderUI size={100} text={"Loading lands..."}/>
+                }
+              </>
               :
-              <LoaderUI size={100} text={"Loading lands..."}/>
+              <LoaderUI size={100} text={"waiting for user signature..."} />
           }
         </>
       }
