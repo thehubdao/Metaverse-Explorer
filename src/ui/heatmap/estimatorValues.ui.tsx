b0881bf895e3ba510f243b607e7c175f635008a4
FrancyAPinzonRNova <santiago.marquez@thedac.info>
2023-11-02 15:42:42 -0500

    hide estimator values and TODO added

--- src/ui/heatmap/estimatorValues.ui.tsx
+++ src/ui/heatmap/estimatorValues.ui.tsx
@@ -5,19 +5,20 @@ import { MetaverseGlobalData } from "../../interfaces/itrm/land-valuation.interf
 interface EstimatorValuesUIProps {
   metaverseSelected: Metaverses;
   info: string;
-  globalData: MetaverseGlobalData | null;
+  globalData?: MetaverseGlobalData | null;
 }
 
-export default function EstimatorValuesUI({ metaverseSelected, info, globalData }: EstimatorValuesUIProps) {
+export default function EstimatorValuesUI({ metaverseSelected, info }: EstimatorValuesUIProps) {
   return (
     <div className="flex flex-wrap items-center justify-between py-8 mt-7">
-      <div className="flex flex-col gap-y-3 w-full lg:w-1/2 lg:max-w-[789px] text-lm-text dark:text-nm-highlight">
+      <div className="flex flex-col gap-y-3 w-full lg:w-1/2 text-lm-text dark:text-nm-highlight">
         <div className='flex gap-x-2 items-end'>
           <p className="text-3xl font-semibold leading-none">{METAVERSE_LABEL[metaverseSelected]}</p>
         </div>
         <p className="text-base font-normal">{info}</p>
       </div>
-      <div className="flex flex-wrap gap-x-8 items-center justify-evenly w-full lg:w-1/2 lg:max-w-2xl">
+      {/* TODO: fix estimator values */}
+      {/* <div className="flex flex-wrap gap-x-8 items-center justify-evenly w-full lg:w-1/2 lg:max-w-2xl">
         <div className="flex flex-col gap-y-1 items-center my-2">
           <p className="font-bold text-xl lg:text-3xl whitespace-nowrap text-lm-text dark:text-nm-highlight">
             {globalData?.floor_price !== undefined ? globalData.floor_price.toLocaleString() : 'No data'}
@@ -45,7 +46,7 @@ export default function EstimatorValuesUI({ metaverseSelected, info, globalData 
           </p>
           <p className="text-xs lg:text-sm font-bold text-lm-text-gray">OWNERS</p>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
