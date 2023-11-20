be179e7052a47feebb3e5ec43c89c069dd050020
smarquezmgh <santiago.marquez@thedac.info>
2023-11-20 13:00:07 -0500

    fixed loading for heatmap component

--- src/ui/heatmap/heatmap.ui.tsx
+++ src/ui/heatmap/heatmap.ui.tsx
@@ -177,7 +177,7 @@ export default function HeatmapUI({ globalData }: HeatmapUIProps) {
                         </button>
                       </div>
                     }
-                  <Heatmap2D metaverse={metaverseSelected} renderAfter={false} onClickLand={(land: LandTileData) => onClickLand(land)} initialX={0} initialY={0} x={coordinates.x} y={coordinates.y} filter={MapFilterEnum.basic}/>
+                    <Heatmap2D metaverse={metaverseSelected} renderAfter={true} onClickLand={(land: LandTileData) => onClickLand(land)} initialX={0} initialY={0} x={coordinates.x} y={coordinates.y} filter={MapFilterEnum.basic}/>
                     {
                       !isVisible &&
                       <MapLegendUI legendFilter={legendFilter} setLegendFilter={(legend: LegendFilter | undefined) => setLegendFilter(legend)} metaverse={metaverseSelected} />
